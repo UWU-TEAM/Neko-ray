a465ae1af003095ab0fcac7e4337b19cf59501be
Blawuken <jarbull87@gmail.com>
2024-04-04 22:56:38 +0700

    Recode libversion with kotlin

---
+++ app/src/main/java/com/neko/marquee/text/LibVersions.kt
@@ -0,0 +1,33 @@
+package com.neko.marquee.text
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.v2ray.ang.util.SpeedtestUtil
+
+class LibVersions(context: Context) : AppCompatTextView(context) {
+    private var memekVersion: String? = null
+
+    private fun JupokInfoSlur() {
+        val sb = StringBuilder()
+        sb.append(SpeedtestUtil.getLibVersion())
+        text = sb.toString()
+    }
+
+    override fun isFocused(): Boolean {
+        return true
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context) {
+        JupokInfoSlur()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs) {
+        JupokInfoSlur()
+    }
+
+    init {
+        JupokInfoSlur()
+    }
+}
+
