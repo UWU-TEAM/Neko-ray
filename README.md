ea6673d76972896a7c85d920836fce2374702f03
developer <dev7developers@gmail.com>
2024-04-09 01:09:20 +0330

    Applied the required changes to convert uri to json

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 * gomobile
 
 ## Build instructions
-1. `install java 17+ , android sdk 34 and ndk 26.2, go 1.22`
+1. `install java 17+ , android sdk 34 and ndk 26.2, go 1.22` # don`t forget to set path!
 2. `go install golang.org/x/mobile/cmd/gomobile@latest`
 3. `go install golang.org/x/mobile/cmd/gobind@latest`
 4. `git clone [repo] && cd AndroidLibXrayLite`
@@ -16,4 +16,6 @@
 7. `go mod tidy -v`
 8. `gomobile bind -v -androidapi 21 -ldflags='-s -w' ./`
 
-This version is different from the original version and has some customization for use in https://github.com/dev7dev/V2ray-Android.
+## Dev7 customizations
+This version is different from the original version and has some customization for use in [V2ray-Android](https://github.com/dev7dev/V2ray-Android).
+It has been tried to convert uri to json inside the library by using [gvcgo/vpnparser](https://github.com/gvcgo/vpnparser) module, plus Some changes for personalization 
--- dev7_additationals.go
+++ dev7_additationals.go
@@ -1,5 +1,64 @@
 package libv2ray
 
-func IsConfigXrayURI(config string) bool {
-	return false
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/gvcgo/vpnparser/pkgs/outbound"
+	"github.com/gvcgo/vpnparser/pkgs/parser"
+	"github.com/gvcgo/vpnparser/pkgs/utils"
+	"strings"
+)
+
+func getOutboundJSONIntended(oStr string) string {
+	var out bytes.Buffer
+	err := json.Indent(&out, []byte(oStr), "", "  ")
+	if err != nil {
+		return oStr
+	}
+	return string(out.Bytes())
+}
+
+func getVMESSUriAttrs(oStr string) string {
+	data, err := base64.StdEncoding.DecodeString(oStr)
+	if err != nil {
+		return oStr
+	}
+	return string(data)
+}
+
+func IsXrayURI(config string) bool {
+	scheme := utils.ParseScheme(config)
+	switch scheme {
+	case parser.SchemeVmess:
+		return true
+	case parser.SchemeVless:
+		return true
+	case parser.SchemeTrojan:
+		return true
+	case parser.SchemeSS:
+		return true
+	default:
+		return false
+	}
+}
+
+func GetXrayOutboundFromURI(rawURI string) string {
+	scheme := utils.ParseScheme(rawURI)
+	if scheme == "" {
+		return ""
+	}
+	if scheme == parser.SchemeVmess {
+		baseVMESSUri := getVMESSUriAttrs(strings.Replace(rawURI, parser.SchemeVmess, "", 1))
+		rawURI = parser.SchemeVmess + baseVMESSUri
+	}
+	ob := outbound.GetOutbound(outbound.XrayCore, rawURI)
+	if ob != nil {
+		ob.Parse(rawURI)
+		return getOutboundJSONIntended(ob.GetOutboundStr())
+	} else {
+		fmt.Println("this is not parsable uri or not supported by this library.")
+	}
+	return ""
 }
