2dust/v2rayNG

Add backup and restore configuration functionality

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/AboutActivity.kt
@@ -1,15 +1,27 @@
 package com.v2ray.ang.ui
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.tbruyelle.rxpermissions.RxPermissions
+import com.tencent.mmkv.MMKV
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityAboutBinding
+import com.v2ray.ang.extension.toast
 import com.v2ray.ang.util.SpeedtestUtil
 import com.v2ray.ang.util.Utils
+import com.v2ray.ang.util.ZipUtil
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class AboutActivity : BaseActivity() {
     private lateinit var binding: ActivityAboutBinding
+    private val extDir by lazy { File(Utils.backupPath(this)) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +30,31 @@ class AboutActivity : BaseActivity() {
         setContentView(view)
 
         title = getString(R.string.title_about)
+
+        binding.tvBackupSummary.text = this.getString(R.string.summary_configuration_backup, extDir)
+        binding.layoutBackup.setOnClickListener {
+            backupMMKV()
+        }
+
+        binding.layoutRestore.setOnClickListener {
+            val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Manifest.permission.READ_MEDIA_IMAGES
+            } else {
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            }
+            RxPermissions(this)
+                .request(permission)
+                .subscribe {
+                    if (it) {
+                        try {
+                            showFileChooser()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    } else
+                        toast(R.string.toast_permission_denied)
+                }
+        }
 
         binding.layoutSoureCcode.setOnClickListener {
             Utils.openUri(this, AppConfig.v2rayNGUrl)
@@ -38,9 +75,79 @@ class AboutActivity : BaseActivity() {
         "v${BuildConfig.VERSION_NAME} (${SpeedtestUtil.getLibVersion()})".also {
             binding.tvVersion.text = it
         }
-
-
     }
 
+    fun backupMMKV() {
+        val dateFormated = SimpleDateFormat(
+            "yyyy-MM-dd-HH-mm-ss",
+            Locale.getDefault()
+        ).format(System.currentTimeMillis())
+        val folderName = "${getString(R.string.app_name)}_${dateFormated}"
+        val backupDir = this.cacheDir.absolutePath + "/$folderName"
+        val outputZipFilePath = extDir.absolutePath + "/$folderName.zip"
 
+        val count = MMKV.backupAllToDirectory(backupDir)
+        if (count <= 0) {
+            toast(R.string.toast_failure)
+        }
+
+        if (ZipUtil.zipFromFolder(backupDir, outputZipFilePath)) {
+            toast(R.string.toast_success)
+        } else {
+            toast(R.string.toast_failure)
+        }
+    }
+
+    fun restoreMMKV(zipFile: File) {
+        val backupDir = this.cacheDir.absolutePath + "/${System.currentTimeMillis()}"
+
+        if (!ZipUtil.unzipToFolder(zipFile, backupDir)) {
+            toast(R.string.toast_failure)
+        }
+
+        val count = MMKV.restoreAllFromDirectory(backupDir)
+        if (count > 0) {
+            toast(R.string.toast_success)
+        } else {
+            toast(R.string.toast_failure)
+        }
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            chooseFile.launch(Intent.createChooser(intent, getString(R.string.title_file_chooser)))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            toast(R.string.toast_require_file_manager)
+        }
+    }
+
+    private val chooseFile =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val uri = it.data?.data
+            if (it.resultCode == RESULT_OK && uri != null) {
+                try {
+                    try {
+                        val targetFile =
+                            File(this.cacheDir.absolutePath, "${System.currentTimeMillis()}.zip")
+                        contentResolver.openInputStream(uri).use { input ->
+                            targetFile.outputStream().use { fileOut ->
+                                input?.copyTo(fileOut)
+                            }
+                        }
+
+                        restoreMMKV(targetFile)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    toast(e.message.toString())
+                }
+            }
+        }
 }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/ZipUtil.kt
@@ -1,0 +1,102 @@
+package com.v2ray.ang.util
+
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
+
+object ZipUtil {
+    private const val BUFFER_SIZE = 4096
+
+    @Throws(IOException::class)
+    fun zipFromFolder(folderPath: String, outputZipFilePath: String): Boolean {
+        val buffer = ByteArray(BUFFER_SIZE)
+
+        try {
+            if (folderPath.isEmpty() || outputZipFilePath.isEmpty()) {
+                return false
+            }
+
+            val filesToCompress = ArrayList<String>()
+            val directory = File(folderPath)
+            if (directory.isDirectory) {
+                directory.listFiles()?.forEach {
+                    if (it.isFile) {
+                        filesToCompress.add(it.absolutePath)
+                    }
+                }
+            }
+            if (filesToCompress.isEmpty()) {
+                return false
+            }
+
+            val zos = ZipOutputStream(FileOutputStream(outputZipFilePath))
+
+            filesToCompress.forEach { file ->
+                val ze = ZipEntry(File(file).name)
+                zos.putNextEntry(ze)
+                val inputStream = FileInputStream(file)
+                while (true) {
+                    val len = inputStream.read(buffer)
+                    if (len <= 0) break
+                    zos.write(buffer, 0, len)
+                }
+
+                inputStream.close()
+            }
+
+            zos.closeEntry()
+            zos.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    @Throws(IOException::class)
+    fun unzipToFolder(zipFile: File, destDirectory: String): Boolean {
+        File(destDirectory).run {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+        try {
+            ZipFile(zipFile).use { zip ->
+                zip.entries().asSequence().forEach { entry ->
+                    zip.getInputStream(entry).use { input ->
+                        val filePath = destDirectory + File.separator + entry.name
+                        if (!entry.isDirectory) {
+                            // if the entry is a file, extracts it
+                            extractFile(input, filePath)
+                        } else {
+                            // if the entry is a directory, make the directory
+                            val dir = File(filePath)
+                            dir.mkdir()
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    @Throws(IOException::class)
+    private fun extractFile(inputStream: InputStream, destFilePath: String) {
+        val bos = BufferedOutputStream(FileOutputStream(destFilePath))
+        val bytesIn = ByteArray(BUFFER_SIZE)
+        var read: Int
+        while (inputStream.read(bytesIn).also { read = it } != -1) {
+            bos.write(bytesIn, 0, read)
+        }
+        bos.close()
+    }
+}

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/ui/AboutActivity.kt
@@ -1,15 +1,27 @@
 package com.neko.v2ray.ui
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.tbruyelle.rxpermissions.RxPermissions
+import com.tencent.mmkv.MMKV
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityAboutBinding
+import com.neko.v2ray.extension.toast
 import com.neko.v2ray.util.SpeedtestUtil
 import com.neko.v2ray.util.Utils
+import com.neko.v2ray.util.ZipUtil
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class AboutActivity : BaseActivity() {
     private lateinit var binding: ActivityAboutBinding
+    private val extDir by lazy { File(Utils.backupPath(this)) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +30,31 @@ class AboutActivity : BaseActivity() {
         setContentView(view)
 
         title = getString(R.string.title_about)
+
+        binding.tvBackupSummary.text = this.getString(R.string.summary_configuration_backup, extDir)
+        binding.layoutBackup.setOnClickListener {
+            backupMMKV()
+        }
+
+        binding.layoutRestore.setOnClickListener {
+            val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Manifest.permission.READ_MEDIA_IMAGES
+            } else {
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            }
+            RxPermissions(this)
+                .request(permission)
+                .subscribe {
+                    if (it) {
+                        try {
+                            showFileChooser()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    } else
+                        toast(R.string.toast_permission_denied)
+                }
+        }
 
         binding.layoutSoureCcode.setOnClickListener {
             Utils.openUri(this, AppConfig.v2rayNGUrl)
@@ -38,9 +75,79 @@ class AboutActivity : BaseActivity() {
         "v${BuildConfig.VERSION_NAME} (${SpeedtestUtil.getLibVersion()})".also {
             binding.tvVersion.text = it
         }
-
-
     }
 
+    fun backupMMKV() {
+        val dateFormated = SimpleDateFormat(
+            "yyyy-MM-dd-HH-mm-ss",
+            Locale.getDefault()
+        ).format(System.currentTimeMillis())
+        val folderName = "${getString(R.string.app_name)}_${dateFormated}"
+        val backupDir = this.cacheDir.absolutePath + "/$folderName"
+        val outputZipFilePath = extDir.absolutePath + "/$folderName.zip"
 
+        val count = MMKV.backupAllToDirectory(backupDir)
+        if (count <= 0) {
+            toast(R.string.toast_failure)
+        }
+
+        if (ZipUtil.zipFromFolder(backupDir, outputZipFilePath)) {
+            toast(R.string.toast_success)
+        } else {
+            toast(R.string.toast_failure)
+        }
+    }
+
+    fun restoreMMKV(zipFile: File) {
+        val backupDir = this.cacheDir.absolutePath + "/${System.currentTimeMillis()}"
+
+        if (!ZipUtil.unzipToFolder(zipFile, backupDir)) {
+            toast(R.string.toast_failure)
+        }
+
+        val count = MMKV.restoreAllFromDirectory(backupDir)
+        if (count > 0) {
+            toast(R.string.toast_success)
+        } else {
+            toast(R.string.toast_failure)
+        }
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+        try {
+            chooseFile.launch(Intent.createChooser(intent, getString(R.string.title_file_chooser)))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            toast(R.string.toast_require_file_manager)
+        }
+    }
+
+    private val chooseFile =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val uri = it.data?.data
+            if (it.resultCode == RESULT_OK && uri != null) {
+                try {
+                    try {
+                        val targetFile =
+                            File(this.cacheDir.absolutePath, "${System.currentTimeMillis()}.zip")
+                        contentResolver.openInputStream(uri).use { input ->
+                            targetFile.outputStream().use { fileOut ->
+                                input?.copyTo(fileOut)
+                            }
+                        }
+
+                        restoreMMKV(targetFile)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    toast(e.message.toString())
+                }
+            }
+        }
 }

app/src/main/kotlin/com/neko/v2ray/util/ZipUtil.kt
@@ -1,0 +1,102 @@
+package com.neko.v2ray.util
+
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
+
+object ZipUtil {
+    private const val BUFFER_SIZE = 4096
+
+    @Throws(IOException::class)
+    fun zipFromFolder(folderPath: String, outputZipFilePath: String): Boolean {
+        val buffer = ByteArray(BUFFER_SIZE)
+
+        try {
+            if (folderPath.isEmpty() || outputZipFilePath.isEmpty()) {
+                return false
+            }
+
+            val filesToCompress = ArrayList<String>()
+            val directory = File(folderPath)
+            if (directory.isDirectory) {
+                directory.listFiles()?.forEach {
+                    if (it.isFile) {
+                        filesToCompress.add(it.absolutePath)
+                    }
+                }
+            }
+            if (filesToCompress.isEmpty()) {
+                return false
+            }
+
+            val zos = ZipOutputStream(FileOutputStream(outputZipFilePath))
+
+            filesToCompress.forEach { file ->
+                val ze = ZipEntry(File(file).name)
+                zos.putNextEntry(ze)
+                val inputStream = FileInputStream(file)
+                while (true) {
+                    val len = inputStream.read(buffer)
+                    if (len <= 0) break
+                    zos.write(buffer, 0, len)
+                }
+
+                inputStream.close()
+            }
+
+            zos.closeEntry()
+            zos.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    @Throws(IOException::class)
+    fun unzipToFolder(zipFile: File, destDirectory: String): Boolean {
+        File(destDirectory).run {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+        try {
+            ZipFile(zipFile).use { zip ->
+                zip.entries().asSequence().forEach { entry ->
+                    zip.getInputStream(entry).use { input ->
+                        val filePath = destDirectory + File.separator + entry.name
+                        if (!entry.isDirectory) {
+                            // if the entry is a file, extracts it
+                            extractFile(input, filePath)
+                        } else {
+                            // if the entry is a directory, make the directory
+                            val dir = File(filePath)
+                            dir.mkdir()
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
+
+    @Throws(IOException::class)
+    private fun extractFile(inputStream: InputStream, destFilePath: String) {
+        val bos = BufferedOutputStream(FileOutputStream(destFilePath))
+        val bytesIn = ByteArray(BUFFER_SIZE)
+        var read: Int
+        while (inputStream.read(bytesIn).also { read = it } != -1) {
+            bos.write(bytesIn, 0, read)
+        }
+        bos.close()
+    }
+}