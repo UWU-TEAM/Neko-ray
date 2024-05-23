1348e1e4dd03d5712e6c65211acc01ccbc15ca2c
Blawuken <jarbull87@gmail.com>
2024-05-24 05:11:16 +0700

    Update mikepenz library

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("com.android.application") version "8.4.0" apply false
     id("com.android.library") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.1.4" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "11.2.0" apply false
 }
