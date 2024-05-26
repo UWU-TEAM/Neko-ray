e8f45bf39e9b0f1c1fb611d9796f92493752a11e
Blawuken <jarbull87@gmail.com>
2024-05-27 01:19:31 +0700

    Added NekoCircleImageView library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -184,7 +184,6 @@ dependencies {
     implementation("androidx.work:work-multiprocess:2.8.1")
 
     // Misc
-    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jsoup:jsoup:1.17.2")
     implementation("androidx.webkit:webkit:1.11.0")
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -54,6 +54,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("org.jsoup:jsoup:1.17.2")
 }
