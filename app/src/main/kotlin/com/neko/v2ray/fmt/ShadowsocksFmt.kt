2dust/v2rayNG

Refactor the parser

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/fmt/ShadowsocksFmt.kt
@@ -1,0 +1,159 @@
+package com.v2ray.ang.util.fmt
+
+import android.util.Log
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.ServerConfig
+import com.v2ray.ang.extension.idnHost
+import com.v2ray.ang.util.Utils
+import java.net.URI
+
+object ShadowsocksFmt {
+    fun parseShadowsocks(str: String): ServerConfig? {
+        val config = ServerConfig.create(EConfigType.SHADOWSOCKS)
+        if (!tryResolveResolveSip002(str, config)) {
+            var result = str.replace(EConfigType.SHADOWSOCKS.protocolScheme, "")
+            val indexSplit = result.indexOf("#")
+            if (indexSplit > 0) {
+                try {
+                    config.remarks =
+                        Utils.urlDecode(result.substring(indexSplit + 1, result.length))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+                result = result.substring(0, indexSplit)
+            }
+
+            //part decode
+            val indexS = result.indexOf("@")
+            result = if (indexS > 0) {
+                Utils.decode(result.substring(0, indexS)) + result.substring(
+                    indexS,
+                    result.length
+                )
+            } else {
+                Utils.decode(result)
+            }
+
+            val legacyPattern = "^(.+?):(.*)@(.+?):(\\d+?)/?$".toRegex()
+            val match = legacyPattern.matchEntire(result)
+                ?: return null
+
+            config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+                server.address = match.groupValues[3].removeSurrounding("[", "]")
+                server.port = match.groupValues[4].toInt()
+                server.password = match.groupValues[2]
+                server.method = match.groupValues[1].lowercase()
+            }
+        }
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val pw =
+            Utils.encode("${outbound.getSecurityEncryption()}:${outbound.getPassword()}")
+        val url = String.format(
+            "%s@%s:%s",
+            pw,
+            Utils.getIpv6Address(outbound.getServerAddress()!!),
+            outbound.getServerPort()
+        )
+        return url + remark
+    }
+
+    private fun tryResolveResolveSip002(str: String, config: ServerConfig): Boolean {
+        try {
+            val uri = URI(Utils.fixIllegalUrl(str))
+            config.remarks = Utils.urlDecode(uri.fragment ?: "")
+
+            val method: String
+            val password: String
+            if (uri.userInfo.contains(":")) {
+                val arrUserInfo = uri.userInfo.split(":").map { it.trim() }
+                if (arrUserInfo.count() != 2) {
+                    return false
+                }
+                method = arrUserInfo[0]
+                password = Utils.urlDecode(arrUserInfo[1])
+            } else {
+                val base64Decode = Utils.decode(uri.userInfo)
+                val arrUserInfo = base64Decode.split(":").map { it.trim() }
+                if (arrUserInfo.count() < 2) {
+                    return false
+                }
+                method = arrUserInfo[0]
+                password = base64Decode.substringAfter(":")
+            }
+
+            val query = Utils.urlDecode(uri.query ?: "")
+            if (query != "") {
+                val queryPairs = HashMap<String, String>()
+                val pairs = query.split(";")
+                Log.d(AppConfig.ANG_PACKAGE, pairs.toString())
+                for (pair in pairs) {
+                    val idx = pair.indexOf("=")
+                    if (idx == -1) {
+                        queryPairs[Utils.urlDecode(pair)] = "";
+                    } else {
+                        queryPairs[Utils.urlDecode(pair.substring(0, idx))] =
+                            Utils.urlDecode(pair.substring(idx + 1))
+                    }
+                }
+                Log.d(AppConfig.ANG_PACKAGE, queryPairs.toString())
+                var sni: String? = ""
+                if (queryPairs["plugin"] == "obfs-local" && queryPairs["obfs"] == "http") {
+                    sni = config.outboundBean?.streamSettings?.populateTransportSettings(
+                        "tcp",
+                        "http",
+                        queryPairs["obfs-host"],
+                        queryPairs["path"],
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                } else if (queryPairs["plugin"] == "v2ray-plugin") {
+                    var network = "ws";
+                    if (queryPairs["mode"] == "quic") {
+                        network = "quic";
+                    }
+                    sni = config.outboundBean?.streamSettings?.populateTransportSettings(
+                        network,
+                        null,
+                        queryPairs["host"],
+                        queryPairs["path"],
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                }
+                if ("tls" in queryPairs) {
+                    config.outboundBean?.streamSettings?.populateTlsSettings(
+                        "tls", false, sni ?: "", null, null, null, null, null
+                    )
+                }
+
+            }
+
+            config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+                server.address = uri.idnHost
+                server.port = uri.port
+                server.password = password
+                server.method = method
+            }
+            return true
+        } catch (e: Exception) {
+            Log.d(AppConfig.ANG_PACKAGE, e.toString())
+            return false
+        }
+    }
+
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/fmt/SocksFmt.kt
@@ -1,0 +1,69 @@
+package com.v2ray.ang.util.fmt
+
+import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.ServerConfig
+import com.v2ray.ang.dto.V2rayConfig
+import com.v2ray.ang.util.Utils
+
+object SocksFmt {
+    fun parseSocks(str: String): ServerConfig? {
+        val config = ServerConfig.create(EConfigType.SOCKS)
+        var result = str.replace(EConfigType.SOCKS.protocolScheme, "")
+        val indexSplit = result.indexOf("#")
+
+        if (indexSplit > 0) {
+            try {
+                config.remarks =
+                    Utils.urlDecode(result.substring(indexSplit + 1, result.length))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            result = result.substring(0, indexSplit)
+        }
+
+        //part decode
+        val indexS = result.indexOf("@")
+        if (indexS > 0) {
+            result = Utils.decode(result.substring(0, indexS)) + result.substring(
+                indexS,
+                result.length
+            )
+        } else {
+            result = Utils.decode(result)
+        }
+
+        val legacyPattern = "^(.*):(.*)@(.+?):(\\d+?)$".toRegex()
+        val match =
+            legacyPattern.matchEntire(result) ?: return null
+
+        config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+            server.address = match.groupValues[3].removeSurrounding("[", "]")
+            server.port = match.groupValues[4].toInt()
+            val socksUsersBean =
+                V2rayConfig.OutboundBean.OutSettingsBean.ServersBean.SocksUsersBean()
+            socksUsersBean.user = match.groupValues[1]
+            socksUsersBean.pass = match.groupValues[2]
+            server.users = listOf(socksUsersBean)
+        }
+
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val pw =
+            if (outbound.settings?.servers?.get(0)?.users?.get(0)?.user != null)
+                "${outbound.settings?.servers?.get(0)?.users?.get(0)?.user}:${outbound.getPassword()}"
+            else
+                ":"
+        val url = String.format(
+            "%s@%s:%s",
+            Utils.encode(pw),
+            Utils.getIpv6Address(outbound.getServerAddress()!!),
+            outbound.getServerPort()
+        )
+        return url + remark
+    }
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/fmt/WireguardFmt.kt
@@ -1,0 +1,71 @@
+package com.v2ray.ang.util.fmt
+
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.ServerConfig
+import com.v2ray.ang.extension.idnHost
+import com.v2ray.ang.extension.removeWhiteSpace
+import com.v2ray.ang.util.Utils
+import java.net.URI
+
+object WireguardFmt {
+    fun parseWireguard(str: String): ServerConfig? {
+        val config = ServerConfig.create(EConfigType.WIREGUARD)
+        val uri = URI(Utils.fixIllegalUrl(str))
+        config.remarks = Utils.urlDecode(uri.fragment ?: "")
+
+        if (uri.rawQuery != null) {
+            val queryParam = uri.rawQuery.split("&")
+                .associate { it.split("=").let { (k, v) -> k to Utils.urlDecode(v) } }
+
+            config.outboundBean?.settings?.let { wireguard ->
+                wireguard.secretKey = uri.userInfo
+                wireguard.address =
+                    (queryParam["address"]
+                        ?: AppConfig.WIREGUARD_LOCAL_ADDRESS_V4).removeWhiteSpace()
+                        .split(",")
+                wireguard.peers?.get(0)?.publicKey = queryParam["publickey"] ?: ""
+                wireguard.peers?.get(0)?.endpoint = "${uri.idnHost}:${uri.port}"
+                wireguard.mtu = Utils.parseInt(queryParam["mtu"] ?: AppConfig.WIREGUARD_LOCAL_MTU)
+                wireguard.reserved =
+                    (queryParam["reserved"] ?: "0,0,0").removeWhiteSpace().split(",")
+                        .map { it.toInt() }
+            }
+        }
+
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val dicQuery = HashMap<String, String>()
+        dicQuery["publickey"] =
+            Utils.urlEncode(outbound.settings?.peers?.get(0)?.publicKey.toString())
+        if (outbound.settings?.reserved != null) {
+            dicQuery["reserved"] = Utils.urlEncode(
+                Utils.removeWhiteSpace(outbound.settings?.reserved?.joinToString())
+                    .toString()
+            )
+        }
+        dicQuery["address"] = Utils.urlEncode(
+            Utils.removeWhiteSpace((outbound.settings?.address as List<*>).joinToString())
+                .toString()
+        )
+        if (outbound.settings?.mtu != null) {
+            dicQuery["mtu"] = outbound.settings?.mtu.toString()
+        }
+        val query = "?" + dicQuery.toList().joinToString(
+            separator = "&",
+            transform = { it.first + "=" + it.second })
+
+        val url = String.format(
+            "%s@%s:%s",
+            Utils.urlEncode(outbound.getPassword().toString()),
+            Utils.getIpv6Address(outbound.getServerAddress()!!),
+            outbound.getServerPort()
+        )
+        return url + query + remark
+    }
+}

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/fmt/ShadowsocksFmt.kt
@@ -1,0 +1,159 @@
+package com.neko.v2ray.util.fmt
+
+import android.util.Log
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.ServerConfig
+import com.neko.v2ray.extension.idnHost
+import com.neko.v2ray.util.Utils
+import java.net.URI
+
+object ShadowsocksFmt {
+    fun parseShadowsocks(str: String): ServerConfig? {
+        val config = ServerConfig.create(EConfigType.SHADOWSOCKS)
+        if (!tryResolveResolveSip002(str, config)) {
+            var result = str.replace(EConfigType.SHADOWSOCKS.protocolScheme, "")
+            val indexSplit = result.indexOf("#")
+            if (indexSplit > 0) {
+                try {
+                    config.remarks =
+                        Utils.urlDecode(result.substring(indexSplit + 1, result.length))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+                result = result.substring(0, indexSplit)
+            }
+
+            //part decode
+            val indexS = result.indexOf("@")
+            result = if (indexS > 0) {
+                Utils.decode(result.substring(0, indexS)) + result.substring(
+                    indexS,
+                    result.length
+                )
+            } else {
+                Utils.decode(result)
+            }
+
+            val legacyPattern = "^(.+?):(.*)@(.+?):(\\d+?)/?$".toRegex()
+            val match = legacyPattern.matchEntire(result)
+                ?: return null
+
+            config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+                server.address = match.groupValues[3].removeSurrounding("[", "]")
+                server.port = match.groupValues[4].toInt()
+                server.password = match.groupValues[2]
+                server.method = match.groupValues[1].lowercase()
+            }
+        }
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val pw =
+            Utils.encode("${outbound.getSecurityEncryption()}:${outbound.getPassword()}")
+        val url = String.format(
+            "%s@%s:%s",
+            pw,
+            Utils.getIpv6Address(outbound.getServerAddress()!!),
+            outbound.getServerPort()
+        )
+        return url + remark
+    }
+
+    private fun tryResolveResolveSip002(str: String, config: ServerConfig): Boolean {
+        try {
+            val uri = URI(Utils.fixIllegalUrl(str))
+            config.remarks = Utils.urlDecode(uri.fragment ?: "")
+
+            val method: String
+            val password: String
+            if (uri.userInfo.contains(":")) {
+                val arrUserInfo = uri.userInfo.split(":").map { it.trim() }
+                if (arrUserInfo.count() != 2) {
+                    return false
+                }
+                method = arrUserInfo[0]
+                password = Utils.urlDecode(arrUserInfo[1])
+            } else {
+                val base64Decode = Utils.decode(uri.userInfo)
+                val arrUserInfo = base64Decode.split(":").map { it.trim() }
+                if (arrUserInfo.count() < 2) {
+                    return false
+                }
+                method = arrUserInfo[0]
+                password = base64Decode.substringAfter(":")
+            }
+
+            val query = Utils.urlDecode(uri.query ?: "")
+            if (query != "") {
+                val queryPairs = HashMap<String, String>()
+                val pairs = query.split(";")
+                Log.d(AppConfig.ANG_PACKAGE, pairs.toString())
+                for (pair in pairs) {
+                    val idx = pair.indexOf("=")
+                    if (idx == -1) {
+                        queryPairs[Utils.urlDecode(pair)] = "";
+                    } else {
+                        queryPairs[Utils.urlDecode(pair.substring(0, idx))] =
+                            Utils.urlDecode(pair.substring(idx + 1))
+                    }
+                }
+                Log.d(AppConfig.ANG_PACKAGE, queryPairs.toString())
+                var sni: String? = ""
+                if (queryPairs["plugin"] == "obfs-local" && queryPairs["obfs"] == "http") {
+                    sni = config.outboundBean?.streamSettings?.populateTransportSettings(
+                        "tcp",
+                        "http",
+                        queryPairs["obfs-host"],
+                        queryPairs["path"],
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                } else if (queryPairs["plugin"] == "v2ray-plugin") {
+                    var network = "ws";
+                    if (queryPairs["mode"] == "quic") {
+                        network = "quic";
+                    }
+                    sni = config.outboundBean?.streamSettings?.populateTransportSettings(
+                        network,
+                        null,
+                        queryPairs["host"],
+                        queryPairs["path"],
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    )
+                }
+                if ("tls" in queryPairs) {
+                    config.outboundBean?.streamSettings?.populateTlsSettings(
+                        "tls", false, sni ?: "", null, null, null, null, null
+                    )
+                }
+
+            }
+
+            config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+                server.address = uri.idnHost
+                server.port = uri.port
+                server.password = password
+                server.method = method
+            }
+            return true
+        } catch (e: Exception) {
+            Log.d(AppConfig.ANG_PACKAGE, e.toString())
+            return false
+        }
+    }
+
+}

app/src/main/kotlin/com/neko/v2ray/fmt/SocksFmt.kt
@@ -1,0 +1,69 @@
+package com.neko.v2ray.util.fmt
+
+import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.ServerConfig
+import com.neko.v2ray.dto.V2rayConfig
+import com.neko.v2ray.util.Utils
+
+object SocksFmt {
+    fun parseSocks(str: String): ServerConfig? {
+        val config = ServerConfig.create(EConfigType.SOCKS)
+        var result = str.replace(EConfigType.SOCKS.protocolScheme, "")
+        val indexSplit = result.indexOf("#")
+
+        if (indexSplit > 0) {
+            try {
+                config.remarks =
+                    Utils.urlDecode(result.substring(indexSplit + 1, result.length))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            result = result.substring(0, indexSplit)
+        }
+
+        //part decode
+        val indexS = result.indexOf("@")
+        if (indexS > 0) {
+            result = Utils.decode(result.substring(0, indexS)) + result.substring(
+                indexS,
+                result.length
+            )
+        } else {
+            result = Utils.decode(result)
+        }
+
+        val legacyPattern = "^(.*):(.*)@(.+?):(\\d+?)$".toRegex()
+        val match =
+            legacyPattern.matchEntire(result) ?: return null
+
+        config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+            server.address = match.groupValues[3].removeSurrounding("[", "]")
+            server.port = match.groupValues[4].toInt()
+            val socksUsersBean =
+                V2rayConfig.OutboundBean.OutSettingsBean.ServersBean.SocksUsersBean()
+            socksUsersBean.user = match.groupValues[1]
+            socksUsersBean.pass = match.groupValues[2]
+            server.users = listOf(socksUsersBean)
+        }
+
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val pw =
+            if (outbound.settings?.servers?.get(0)?.users?.get(0)?.user != null)
+                "${outbound.settings?.servers?.get(0)?.users?.get(0)?.user}:${outbound.getPassword()}"
+            else
+                ":"
+        val url = String.format(
+            "%s@%s:%s",
+            Utils.encode(pw),
+            Utils.getIpv6Address(outbound.getServerAddress()!!),
+            outbound.getServerPort()
+        )
+        return url + remark
+    }
+}

app/src/main/kotlin/com/neko/v2ray/fmt/WireguardFmt.kt
@@ -1,0 +1,71 @@
+package com.neko.v2ray.util.fmt
+
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.ServerConfig
+import com.neko.v2ray.extension.idnHost
+import com.neko.v2ray.extension.removeWhiteSpace
+import com.neko.v2ray.util.Utils
+import java.net.URI
+
+object WireguardFmt {
+    fun parseWireguard(str: String): ServerConfig? {
+        val config = ServerConfig.create(EConfigType.WIREGUARD)
+        val uri = URI(Utils.fixIllegalUrl(str))
+        config.remarks = Utils.urlDecode(uri.fragment ?: "")
+
+        if (uri.rawQuery != null) {
+            val queryParam = uri.rawQuery.split("&")
+                .associate { it.split("=").let { (k, v) -> k to Utils.urlDecode(v) } }
+
+            config.outboundBean?.settings?.let { wireguard ->
+                wireguard.secretKey = uri.userInfo
+                wireguard.address =
+                    (queryParam["address"]
+                        ?: AppConfig.WIREGUARD_LOCAL_ADDRESS_V4).removeWhiteSpace()
+                        .split(",")
+                wireguard.peers?.get(0)?.publicKey = queryParam["publickey"] ?: ""
+                wireguard.peers?.get(0)?.endpoint = "${uri.idnHost}:${uri.port}"
+                wireguard.mtu = Utils.parseInt(queryParam["mtu"] ?: AppConfig.WIREGUARD_LOCAL_MTU)
+                wireguard.reserved =
+                    (queryParam["reserved"] ?: "0,0,0").removeWhiteSpace().split(",")
+                        .map { it.toInt() }
+            }
+        }
+
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val dicQuery = HashMap<String, String>()
+        dicQuery["publickey"] =
+            Utils.urlEncode(outbound.settings?.peers?.get(0)?.publicKey.toString())
+        if (outbound.settings?.reserved != null) {
+            dicQuery["reserved"] = Utils.urlEncode(
+                Utils.removeWhiteSpace(outbound.settings?.reserved?.joinToString())
+                    .toString()
+            )
+        }
+        dicQuery["address"] = Utils.urlEncode(
+            Utils.removeWhiteSpace((outbound.settings?.address as List<*>).joinToString())
+                .toString()
+        )
+        if (outbound.settings?.mtu != null) {
+            dicQuery["mtu"] = outbound.settings?.mtu.toString()
+        }
+        val query = "?" + dicQuery.toList().joinToString(
+            separator = "&",
+            transform = { it.first + "=" + it.second })
+
+        val url = String.format(
+            "%s@%s:%s",
+            Utils.urlEncode(outbound.getPassword().toString()),
+            Utils.getIpv6Address(outbound.getServerAddress()!!),
+            outbound.getServerPort()
+        )
+        return url + query + remark
+    }
+}