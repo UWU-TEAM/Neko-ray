2dust/v2rayNG

Add progress bar when subscription is updated

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/MainActivity.kt
@@ -31,6 +31,7 @@ import com.v2ray.ang.AppConfig
 import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityMainBinding
+import com.v2ray.ang.databinding.LayoutProgressBinding
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.helper.SimpleItemTouchHelperCallback
@@ -39,7 +40,9 @@ import com.v2ray.ang.util.AngConfigManager
 import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.viewmodel.MainViewModel
+import com.v2ray.ang.viewmodel.SubViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import me.drakeet.support.toast.ToastCompat
 import rx.Observable
@@ -61,6 +64,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     }
     private var mItemTouchHelper: ItemTouchHelper? = null
     val mainViewModel: MainViewModel by viewModels()
+    val subViewModel: SubViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -281,11 +285,6 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
             true
         }
 
-//        R.id.sub_setting -> {
-//            startActivity<SubSettingActivity>()
-//            true
-//        }
-
         R.id.sub_update -> {
             importConfigViaSub()
             true
@@ -423,26 +422,24 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         return true
     }
 
-    fun importBatchConfig(server: String?, subid: String = "") {
-        val subid2 = if(subid.isNullOrEmpty()){
-            mainViewModel.subscriptionId
-        }else{
-            subid
-        }
-        val append = subid.isNullOrEmpty()
+    fun importBatchConfig(server: String?) {
+        val dialog = AlertDialog.Builder(this)
+            .setView(LayoutProgressBinding.inflate(layoutInflater).root)
+            .setCancelable(false)
+            .show()
 
-        var count = AngConfigManager.importBatchConfig(server, subid2, append)
-        if (count <= 0) {
-            count = AngConfigManager.importBatchConfig(Utils.decode(server!!), subid2, append)
-        }
-        if (count <= 0) {
-            count = AngConfigManager.appendCustomConfigServer(server, subid2)
-        }
-        if (count > 0) {
-            toast(R.string.toast_success)
-            mainViewModel.reloadServerList()
-        } else {
-            toast(R.string.toast_failure)
+        lifecycleScope.launch(Dispatchers.IO) {
+            val count = subViewModel.importBatchConfig(server, mainViewModel.subscriptionId, true)
+            delay(500L)
+            launch(Dispatchers.Main) {
+                if (count > 0) {
+                    toast(R.string.toast_success)
+                    mainViewModel.reloadServerList()
+                } else {
+                    toast(R.string.toast_failure)
+                }
+                dialog.dismiss()
+            }
         }
     }
 
@@ -520,55 +517,24 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     /**
      * import config from sub
      */
-    fun importConfigViaSub()
-            : Boolean {
-        try {
-            toast(R.string.title_sub_update)
-            MmkvManager.decodeSubscriptions().forEach {
-                if (TextUtils.isEmpty(it.first)
-                        || TextUtils.isEmpty(it.second.remarks)
-                        || TextUtils.isEmpty(it.second.url)
-                ) {
-                    return@forEach
+    fun importConfigViaSub() : Boolean {
+        val dialog = AlertDialog.Builder(this)
+            .setView(LayoutProgressBinding.inflate(layoutInflater).root)
+            .setCancelable(false)
+            .show()
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val count = subViewModel.updateConfigViaSubAll()
+            delay(500L)
+            launch(Dispatchers.Main) {
+                if (count > 0) {
+                    toast(R.string.toast_success)
+                    mainViewModel.reloadServerList()
+                } else {
+                    toast(R.string.toast_failure)
                 }
-                if (!it.second.enabled) {
-                    return@forEach
-                }
-                val url = Utils.idnToASCII(it.second.url)
-                if (!Utils.isValidUrl(url)) {
-                    return@forEach
-                }
-                Log.d(ANG_PACKAGE, url)
-                lifecycleScope.launch(Dispatchers.IO) {
-                    var configText = try {
-                        Utils.getUrlContentWithCustomUserAgent(url)
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        ""
-                    }
-                    if(configText.isEmpty()) {
-                        configText = try {
-                            val httpPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT), AppConfig.PORT_HTTP.toInt())
-                            Utils.getUrlContentWithCustomUserAgent(url, httpPort)
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            ""
-                        }
-                    }
-                    if(configText.isEmpty()) {
-                        launch(Dispatchers.Main) {
-                            toast("\"" + it.second.remarks + "\" " + getString(R.string.toast_failure))
-                        }
-                        return@launch
-                    }
-                    launch(Dispatchers.Main) {
-                        importBatchConfig(configText, it.first)
-                    }
-                }
+                dialog.dismiss()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return false
         }
         return true
     }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingActivity.kt
@@ -1,20 +1,30 @@
 package com.v2ray.ang.ui
 
 import android.content.Intent
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.v2ray.ang.R
-import android.os.Bundle
 import com.v2ray.ang.databinding.ActivitySubSettingBinding
+import com.v2ray.ang.databinding.LayoutProgressBinding
 import com.v2ray.ang.dto.SubscriptionItem
+import com.v2ray.ang.extension.toast
 import com.v2ray.ang.util.MmkvManager
+import com.v2ray.ang.viewmodel.SubViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SubSettingActivity : BaseActivity() {
     private lateinit var binding: ActivitySubSettingBinding
 
-    var subscriptions:List<Pair<String, SubscriptionItem>> = listOf()
+    var subscriptions: List<Pair<String, SubscriptionItem>> = listOf()
     private val adapter by lazy { SubSettingRecyclerAdapter(this) }
+    val subViewModel: SubViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,9 +47,6 @@ class SubSettingActivity : BaseActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.action_sub_setting, menu)
-        menu.findItem(R.id.del_config)?.isVisible = false
-        menu.findItem(R.id.save_config)?.isVisible = false
-
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -48,6 +55,30 @@ class SubSettingActivity : BaseActivity() {
             startActivity(Intent(this, SubEditActivity::class.java))
             true
         }
+
+        R.id.sub_update -> {
+            val dialog = AlertDialog.Builder(this)
+                .setView(LayoutProgressBinding.inflate(layoutInflater).root)
+                .setCancelable(false)
+                .show()
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                val count = subViewModel.updateConfigViaSubAll()
+                delay(500L)
+                launch(Dispatchers.Main) {
+                    if (count > 0) {
+                        toast(R.string.toast_success)
+                    } else {
+                        toast(R.string.toast_failure)
+                    }
+                    dialog.dismiss()
+                }
+            }
+
+            true
+        }
+
         else -> super.onOptionsItemSelected(item)
+
     }
 }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/viewmodel/SubViewModel.kt
@@ -1,0 +1,93 @@
+package com.v2ray.ang.viewmodel
+
+import android.app.Application
+import android.text.TextUtils
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.tencent.mmkv.MMKV
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.dto.SubscriptionItem
+import com.v2ray.ang.util.AngConfigManager
+import com.v2ray.ang.util.MmkvManager
+import com.v2ray.ang.util.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+class SubViewModel(application: Application) : AndroidViewModel(application) {
+    private val settingsStorage by lazy {
+        MMKV.mmkvWithID(
+            MmkvManager.ID_SETTING,
+            MMKV.MULTI_PROCESS_MODE
+        )
+    }
+
+    private val tcpingTestScope by lazy { CoroutineScope(Dispatchers.IO) }
+
+    fun updateConfigViaSubAll(): Int {
+        var count = 0
+        try {
+            MmkvManager.decodeSubscriptions().forEach {
+                count += updateConfigViaSub(it)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return 0
+        }
+        return count
+    }
+
+    fun updateConfigViaSub(it: Pair<String, SubscriptionItem>): Int {
+        try {
+            if (TextUtils.isEmpty(it.first)
+                || TextUtils.isEmpty(it.second.remarks)
+                || TextUtils.isEmpty(it.second.url)
+            ) {
+                return 0
+            }
+            if (!it.second.enabled) {
+                return 0
+            }
+            val url = Utils.idnToASCII(it.second.url)
+            if (!Utils.isValidUrl(url)) {
+                return 0
+            }
+            Log.d(AppConfig.ANG_PACKAGE, url)
+            var configText = try {
+                Utils.getUrlContentWithCustomUserAgent(url)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ""
+            }
+            if (configText.isEmpty()) {
+                configText = try {
+                    val httpPort = Utils.parseInt(
+                        settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT),
+                        AppConfig.PORT_HTTP.toInt()
+                    )
+                    Utils.getUrlContentWithCustomUserAgent(url, httpPort)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    ""
+                }
+            }
+            if (configText.isEmpty()) {
+                return 0
+            }
+            return importBatchConfig(configText, it.first, false)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return 0
+        }
+    }
+
+    fun importBatchConfig(server: String?, subid: String = "", append: Boolean): Int {
+        var count = AngConfigManager.importBatchConfig(server, subid, append)
+        if (count <= 0) {
+            count = AngConfigManager.importBatchConfig(Utils.decode(server!!), subid, append)
+        }
+        if (count <= 0) {
+            count = AngConfigManager.appendCustomConfigServer(server, subid)
+        }
+        return count
+    }
+}

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/ui/MainActivity.kt
@@ -31,6 +31,7 @@ import com.neko.v2ray.AppConfig
 import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityMainBinding
+import com.neko.v2ray.databinding.LayoutProgressBinding
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.helper.SimpleItemTouchHelperCallback
@@ -39,7 +40,9 @@ import com.neko.v2ray.util.AngConfigManager
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.viewmodel.MainViewModel
+import com.neko.v2ray.viewmodel.SubViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import me.drakeet.support.toast.ToastCompat
 import rx.Observable
@@ -82,6 +85,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     private var mItemTouchHelper: ItemTouchHelper? = null
     private var noInternetDialogSignal: NoInternetDialogSignal? = null
     val mainViewModel: MainViewModel by viewModels()
+    val subViewModel: SubViewModel by viewModels()
     val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -346,11 +350,6 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
             true
         }
 
-//        R.id.sub_setting -> {
-//            startActivity<SubSettingActivity>()
-//            true
-//        }
-
         R.id.sub_update -> {
             importConfigViaSub()
             true
@@ -488,26 +487,24 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         return true
     }
 
-    fun importBatchConfig(server: String?, subid: String = "") {
-        val subid2 = if(subid.isNullOrEmpty()){
-            mainViewModel.subscriptionId
-        }else{
-            subid
-        }
-        val append = subid.isNullOrEmpty()
+    fun importBatchConfig(server: String?) {
+        val dialog = AlertDialog.Builder(this)
+            .setView(LayoutProgressBinding.inflate(layoutInflater).root)
+            .setCancelable(false)
+            .show()
 
-        var count = AngConfigManager.importBatchConfig(server, subid2, append)
-        if (count <= 0) {
-            count = AngConfigManager.importBatchConfig(Utils.decode(server!!), subid2, append)
-        }
-        if (count <= 0) {
-            count = AngConfigManager.appendCustomConfigServer(server, subid2)
-        }
-        if (count > 0) {
-            toast(R.string.toast_success)
-            mainViewModel.reloadServerList()
-        } else {
-            toast(R.string.toast_failure)
+        lifecycleScope.launch(Dispatchers.IO) {
+            val count = subViewModel.importBatchConfig(server, mainViewModel.subscriptionId, true)
+            delay(500L)
+            launch(Dispatchers.Main) {
+                if (count > 0) {
+                    toast(R.string.toast_success)
+                    mainViewModel.reloadServerList()
+                } else {
+                    toast(R.string.toast_failure)
+                }
+                dialog.dismiss()
+            }
         }
     }
 
@@ -585,55 +582,24 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     /**
      * import config from sub
      */
-    fun importConfigViaSub()
-            : Boolean {
-        try {
-            toast(R.string.title_sub_update)
-            MmkvManager.decodeSubscriptions().forEach {
-                if (TextUtils.isEmpty(it.first)
-                        || TextUtils.isEmpty(it.second.remarks)
-                        || TextUtils.isEmpty(it.second.url)
-                ) {
-                    return@forEach
+    fun importConfigViaSub() : Boolean {
+        val dialog = AlertDialog.Builder(this)
+            .setView(LayoutProgressBinding.inflate(layoutInflater).root)
+            .setCancelable(false)
+            .show()
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val count = subViewModel.updateConfigViaSubAll()
+            delay(500L)
+            launch(Dispatchers.Main) {
+                if (count > 0) {
+                    toast(R.string.toast_success)
+                    mainViewModel.reloadServerList()
+                } else {
+                    toast(R.string.toast_failure)
                 }
-                if (!it.second.enabled) {
-                    return@forEach
-                }
-                val url = Utils.idnToASCII(it.second.url)
-                if (!Utils.isValidUrl(url)) {
-                    return@forEach
-                }
-                Log.d(ANG_PACKAGE, url)
-                lifecycleScope.launch(Dispatchers.IO) {
-                    var configText = try {
-                        Utils.getUrlContentWithCustomUserAgent(url)
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                        ""
-                    }
-                    if(configText.isEmpty()) {
-                        configText = try {
-                            val httpPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT), AppConfig.PORT_HTTP.toInt())
-                            Utils.getUrlContentWithCustomUserAgent(url, httpPort)
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            ""
-                        }
-                    }
-                    if(configText.isEmpty()) {
-                        launch(Dispatchers.Main) {
-                            toast("\"" + it.second.remarks + "\" " + getString(R.string.toast_failure))
-                        }
-                        return@launch
-                    }
-                    launch(Dispatchers.Main) {
-                        importBatchConfig(configText, it.first)
-                    }
-                }
+                dialog.dismiss()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return false
         }
         return true
     }

app/src/main/kotlin/com/neko/v2ray/ui/SubSettingActivity.kt
@@ -1,22 +1,33 @@
 package com.neko.v2ray.ui
 
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.neko.v2ray.R
-import android.os.Bundle
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.neko.v2ray.databinding.ActivitySubSettingBinding
+import com.neko.v2ray.databinding.LayoutProgressBinding
 import com.neko.v2ray.dto.SubscriptionItem
+import com.neko.v2ray.extension.toast
 import com.neko.v2ray.util.MmkvManager
+import com.neko.v2ray.viewmodel.SubViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SubSettingActivity : BaseActivity() {
     private lateinit var binding: ActivitySubSettingBinding
 
-    var subscriptions:List<Pair<String, SubscriptionItem>> = listOf()
+    var subscriptions: List<Pair<String, SubscriptionItem>> = listOf()
     private val adapter by lazy { SubSettingRecyclerAdapter(this) }
+    val subViewModel: SubViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,9 +53,6 @@ class SubSettingActivity : BaseActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.action_sub_setting, menu)
-        menu.findItem(R.id.del_config)?.isVisible = false
-        menu.findItem(R.id.save_config)?.isVisible = false
-
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -53,6 +61,30 @@ class SubSettingActivity : BaseActivity() {
             startActivity(Intent(this, SubEditActivity::class.java))
             true
         }
+
+        R.id.sub_update -> {
+            val dialog = AlertDialog.Builder(this)
+                .setView(LayoutProgressBinding.inflate(layoutInflater).root)
+                .setCancelable(false)
+                .show()
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                val count = subViewModel.updateConfigViaSubAll()
+                delay(500L)
+                launch(Dispatchers.Main) {
+                    if (count > 0) {
+                        toast(R.string.toast_success)
+                    } else {
+                        toast(R.string.toast_failure)
+                    }
+                    dialog.dismiss()
+                }
+            }
+
+            true
+        }
+
         else -> super.onOptionsItemSelected(item)
+
     }
 }

app/src/main/kotlin/com/neko/v2ray/viewmodel/SubViewModel.kt
@@ -1,0 +1,93 @@
+package com.neko.v2ray.viewmodel
+
+import android.app.Application
+import android.text.TextUtils
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.tencent.mmkv.MMKV
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.dto.SubscriptionItem
+import com.neko.v2ray.util.AngConfigManager
+import com.neko.v2ray.util.MmkvManager
+import com.neko.v2ray.util.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+class SubViewModel(application: Application) : AndroidViewModel(application) {
+    private val settingsStorage by lazy {
+        MMKV.mmkvWithID(
+            MmkvManager.ID_SETTING,
+            MMKV.MULTI_PROCESS_MODE
+        )
+    }
+
+    private val tcpingTestScope by lazy { CoroutineScope(Dispatchers.IO) }
+
+    fun updateConfigViaSubAll(): Int {
+        var count = 0
+        try {
+            MmkvManager.decodeSubscriptions().forEach {
+                count += updateConfigViaSub(it)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return 0
+        }
+        return count
+    }
+
+    fun updateConfigViaSub(it: Pair<String, SubscriptionItem>): Int {
+        try {
+            if (TextUtils.isEmpty(it.first)
+                || TextUtils.isEmpty(it.second.remarks)
+                || TextUtils.isEmpty(it.second.url)
+            ) {
+                return 0
+            }
+            if (!it.second.enabled) {
+                return 0
+            }
+            val url = Utils.idnToASCII(it.second.url)
+            if (!Utils.isValidUrl(url)) {
+                return 0
+            }
+            Log.d(AppConfig.ANG_PACKAGE, url)
+            var configText = try {
+                Utils.getUrlContentWithCustomUserAgent(url)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                ""
+            }
+            if (configText.isEmpty()) {
+                configText = try {
+                    val httpPort = Utils.parseInt(
+                        settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT),
+                        AppConfig.PORT_HTTP.toInt()
+                    )
+                    Utils.getUrlContentWithCustomUserAgent(url, httpPort)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    ""
+                }
+            }
+            if (configText.isEmpty()) {
+                return 0
+            }
+            return importBatchConfig(configText, it.first, false)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return 0
+        }
+    }
+
+    fun importBatchConfig(server: String?, subid: String = "", append: Boolean): Int {
+        var count = AngConfigManager.importBatchConfig(server, subid, append)
+        if (count <= 0) {
+            count = AngConfigManager.importBatchConfig(Utils.decode(server!!), subid, append)
+        }
+        if (count <= 0) {
+            count = AngConfigManager.appendCustomConfigServer(server, subid)
+        }
+        return count
+    }
+}