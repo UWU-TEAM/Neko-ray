7e68e8da3e361455e63989fd2aa5c4c27f79a269
AnGgIt86 <jarbull86@gmail.com>
2024-08-09 23:24:07 +0700

    Add buildToolsVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ configurations {
 android {
     namespace = "com.neko.v2ray"
     compileSdk = 34
+    buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "com.neko.v2ray"
