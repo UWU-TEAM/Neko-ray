2dust/v2rayNG

Update subscriptions based on grouping
https://github.com/2dust/v2rayNG/issues/3445

V2rayNG/app/src/main/kotlin/com/v2ray/ang/viewmodel/MainViewModel.kt
@@ -20,12 +20,15 @@ import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.ServerConfig
 import com.v2ray.ang.dto.ServersCache
+import com.v2ray.ang.dto.SubscriptionItem
 import com.v2ray.ang.dto.V2rayConfig
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.util.AngConfigManager
+import com.v2ray.ang.util.AngConfigManager.updateConfigViaSub
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.MmkvManager.KEY_ANG_CONFIGS
+import com.v2ray.ang.util.MmkvManager.subStorage
 import com.v2ray.ang.util.SpeedtestUtil
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.util.V2rayConfigUtil
@@ -152,6 +155,18 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
+    fun updateConfigViaSubAll(): Int {
+        if (subscriptionId.isNullOrEmpty()) {
+            return AngConfigManager.updateConfigViaSubAll()
+        } else {
+            val json = subStorage?.decodeString(subscriptionId)
+            if (!json.isNullOrBlank()) {
+                return updateConfigViaSub(Pair(subscriptionId, Gson().fromJson(json, SubscriptionItem::class.java)))
+            } else {
+                return 0
+            }
+        }
+    }
 
     fun exportAllServer(): Int {
         val serverListCopy =

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
@@ -20,12 +20,15 @@ import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.ServerConfig
 import com.neko.v2ray.dto.ServersCache
+import com.neko.v2ray.dto.SubscriptionItem
 import com.neko.v2ray.dto.V2rayConfig
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.util.AngConfigManager
+import com.neko.v2ray.util.AngConfigManager.updateConfigViaSub
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.MmkvManager.KEY_ANG_CONFIGS
+import com.neko.v2ray.util.MmkvManager.subStorage
 import com.neko.v2ray.util.SpeedtestUtil
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.util.V2rayConfigUtil
@@ -153,6 +156,18 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
+    fun updateConfigViaSubAll(): Int {
+        if (subscriptionId.isNullOrEmpty()) {
+            return AngConfigManager.updateConfigViaSubAll()
+        } else {
+            val json = subStorage?.decodeString(subscriptionId)
+            if (!json.isNullOrBlank()) {
+                return updateConfigViaSub(Pair(subscriptionId, Gson().fromJson(json, SubscriptionItem::class.java)))
+            } else {
+                return 0
+            }
+        }
+    }
 
     fun exportAllServer(): Int {
         val serverListCopy =