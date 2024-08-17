9d2b81c14473b0d2b8292dda952b8b76bd06b9b3
2dust <31833384+2dust@users.noreply.github.com>
2024-08-17 14:58:48 +0800

    Optimized search

--- app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
@@ -46,7 +46,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     var subscriptionId: String = MmkvManager.settingsStorage.decodeString(AppConfig.CACHE_SUBSCRIPTION_ID, "").orEmpty()
 
     //var keywordFilter: String = MmkvManager.settingsStorage.decodeString(AppConfig.CACHE_KEYWORD_FILTER, "")?:""
-    private var keywordFilter = ""
+    var keywordFilter = ""
     val serversCache = mutableListOf<ServersCache>()
     val isRunning by lazy { MutableLiveData<Boolean>() }
     val updateListAction by lazy { MutableLiveData<Int>() }
@@ -171,7 +171,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     fun exportAllServer(): Int {
         val serverListCopy =
-            if (subscriptionId.isNullOrEmpty()) {
+            if (subscriptionId.isNullOrEmpty() && keywordFilter.isNullOrEmpty()) {
                 serverList
             } else {
                 serversCache.map { it.guid }.toList()
@@ -296,7 +296,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun removeAllServer() {
-        if (subscriptionId.isNullOrEmpty()) {
+        if (subscriptionId.isNullOrEmpty() && keywordFilter.isNullOrEmpty()) {
             MmkvManager.removeAllServer()
         } else {
             val serversCopy = serversCache.toList()
@@ -307,7 +307,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun removeInvalidServer() {
-        if (subscriptionId.isNullOrEmpty()) {
+        if (subscriptionId.isNullOrEmpty() && keywordFilter.isNullOrEmpty()) {
             MmkvManager.removeInvalidServer("")
         } else {
             val serversCopy = serversCache.toList()
@@ -349,6 +349,9 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun filterConfig(keyword: String) {
+        if (keyword == keywordFilter) {
+            return
+        }
         keywordFilter = keyword
         MmkvManager.settingsStorage.encode(AppConfig.CACHE_KEYWORD_FILTER, keywordFilter)
         reloadServerList()
