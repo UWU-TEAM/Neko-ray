2dust/v2rayNG

Add drag-and-drop sorting to grouping

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/RoutingSettingActivity.kt
@@ -8,12 +8,14 @@ import android.view.View
 import android.widget.AdapterView
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityRoutingSettingBinding
 import com.v2ray.ang.dto.RulesetItem
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.helper.SimpleItemTouchHelperCallback
 import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.MmkvManager.settingsStorage
 import com.v2ray.ang.util.SettingsManager
@@ -26,6 +28,7 @@ class RoutingSettingActivity : BaseActivity() {
 
     var rulesets: MutableList<RulesetItem> = mutableListOf()
     private val adapter by lazy { RoutingSettingRecyclerAdapter(this) }
+    private var mItemTouchHelper: ItemTouchHelper? = null
     private val routing_domain_strategy: Array<out String> by lazy {
         resources.getStringArray(R.array.routing_domain_strategy)
     }
@@ -42,6 +45,9 @@ class RoutingSettingActivity : BaseActivity() {
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
+
+        mItemTouchHelper = ItemTouchHelper(SimpleItemTouchHelperCallback(adapter))
+        mItemTouchHelper?.attachToRecyclerView(binding.recyclerView)
 
         val found = Utils.arrayFind(routing_domain_strategy, settingsStorage?.decodeString(AppConfig.PREF_ROUTING_DOMAIN_STRATEGY) ?: "")
         found.let { binding.spDomainStrategy.setSelection(if (it >= 0) it else 0) }
@@ -105,7 +111,7 @@ class RoutingSettingActivity : BaseActivity() {
         else -> super.onOptionsItemSelected(item)
     }
 
-    private fun refreshData() {
+    fun refreshData() {
         rulesets = MmkvManager.decodeRoutingRulesets() ?: mutableListOf()
         adapter.notifyDataSetChanged()
     }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/RoutingSettingRecyclerAdapter.kt
@@ -3,13 +3,17 @@ package com.v2ray.ang.ui
 import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.v2ray.ang.databinding.ItemRecyclerRoutingSettingBinding
+import com.v2ray.ang.helper.ItemTouchHelperAdapter
+import com.v2ray.ang.helper.ItemTouchHelperViewHolder
+import com.v2ray.ang.ui.MainRecyclerAdapter.BaseViewHolder
 import com.v2ray.ang.util.SettingsManager
 
-class RoutingSettingRecyclerAdapter(val activity: RoutingSettingActivity) :
-    RecyclerView.Adapter<RoutingSettingRecyclerAdapter.MainViewHolder>() {
+class RoutingSettingRecyclerAdapter(val activity: RoutingSettingActivity) : RecyclerView.Adapter<RoutingSettingRecyclerAdapter.MainViewHolder>(),
+    ItemTouchHelperAdapter {
 
     private var mActivity: RoutingSettingActivity = activity
     override fun getItemCount() = mActivity.rulesets.size
@@ -47,5 +51,28 @@ class RoutingSettingRecyclerAdapter(val activity: RoutingSettingActivity) :
     }
 
     class MainViewHolder(val itemRoutingSettingBinding: ItemRecyclerRoutingSettingBinding) :
-        RecyclerView.ViewHolder(itemRoutingSettingBinding.root)
+        BaseViewHolder(itemRoutingSettingBinding.root), ItemTouchHelperViewHolder
+
+    open class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY)
+        }
+
+        fun onItemClear() {
+            itemView.setBackgroundColor(0)
+        }
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+        SettingsManager.swapRoutingRuleset(fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
+        return true
+    }
+
+    override fun onItemMoveCompleted() {
+        mActivity.refreshData()
+    }
+
+    override fun onItemDismiss(position: Int) {
+    }
 }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingActivity.kt
@@ -6,12 +6,14 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivitySubSettingBinding
 import com.v2ray.ang.databinding.LayoutProgressBinding
 import com.v2ray.ang.dto.SubscriptionItem
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.helper.SimpleItemTouchHelperCallback
 import com.v2ray.ang.util.AngConfigManager
 import com.v2ray.ang.util.MmkvManager
 import kotlinx.coroutines.Dispatchers
@@ -23,6 +25,7 @@ class SubSettingActivity : BaseActivity() {
 
     var subscriptions: List<Pair<String, SubscriptionItem>> = listOf()
     private val adapter by lazy { SubSettingRecyclerAdapter(this) }
+    private var mItemTouchHelper: ItemTouchHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +36,14 @@ class SubSettingActivity : BaseActivity() {
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
+
+        mItemTouchHelper = ItemTouchHelper(SimpleItemTouchHelperCallback(adapter))
+        mItemTouchHelper?.attachToRecyclerView(binding.recyclerView)
     }
 
     override fun onResume() {
         super.onResume()
-        subscriptions = MmkvManager.decodeSubscriptions()
-        adapter.notifyDataSetChanged()
+        refreshData()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -76,5 +81,10 @@ class SubSettingActivity : BaseActivity() {
 
         else -> super.onOptionsItemSelected(item)
 
+    }
+
+    fun refreshData() {
+        subscriptions = MmkvManager.decodeSubscriptions()
+        adapter.notifyDataSetChanged()
     }
 }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingRecyclerAdapter.kt
@@ -8,17 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ItemQrcodeBinding
 import com.v2ray.ang.databinding.ItemRecyclerSubSettingBinding
 import com.v2ray.ang.extension.toast
-import com.v2ray.ang.util.MmkvManager.subStorage
+import com.v2ray.ang.helper.ItemTouchHelperAdapter
+import com.v2ray.ang.helper.ItemTouchHelperViewHolder
+import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.QRCodeDecoder
+import com.v2ray.ang.util.SettingsManager
 import com.v2ray.ang.util.Utils
 
-class SubSettingRecyclerAdapter(val activity: SubSettingActivity) :
-    RecyclerView.Adapter<SubSettingRecyclerAdapter.MainViewHolder>() {
+class SubSettingRecyclerAdapter(val activity: SubSettingActivity) : RecyclerView.Adapter<SubSettingRecyclerAdapter.MainViewHolder>(), ItemTouchHelperAdapter {
 
     private var mActivity: SubSettingActivity = activity
 
@@ -45,7 +46,8 @@ class SubSettingRecyclerAdapter(val activity: SubSettingActivity) :
 
         holder.itemSubSettingBinding.chkEnable.setOnCheckedChangeListener { _, isChecked ->
             subItem.enabled = isChecked
-            subStorage?.encode(subId, Gson().toJson(subItem))
+            MmkvManager.encodeSubscription(subId, subItem)
+
         }
 
         if (TextUtils.isEmpty(subItem.url)) {
@@ -93,5 +95,28 @@ class SubSettingRecyclerAdapter(val activity: SubSettingActivity) :
     }
 
     class MainViewHolder(val itemSubSettingBinding: ItemRecyclerSubSettingBinding) :
-        RecyclerView.ViewHolder(itemSubSettingBinding.root)
+        BaseViewHolder(itemSubSettingBinding.root), ItemTouchHelperViewHolder
+
+    open class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY)
+        }
+
+        fun onItemClear() {
+            itemView.setBackgroundColor(0)
+        }
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+        SettingsManager.swapSubscriptions(fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
+        return true
+    }
+
+    override fun onItemMoveCompleted() {
+        mActivity.refreshData()
+    }
+
+    override fun onItemDismiss(position: Int) {
+    }
 }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/SettingsManager.kt
@@ -2,8 +2,11 @@ package com.v2ray.ang.util
 
 import android.content.Context
 import android.text.TextUtils
+import android.util.Log
 import com.google.gson.Gson
 import com.v2ray.ang.dto.RulesetItem
+import com.v2ray.ang.util.MmkvManager.subStorage
+import java.util.Collections
 
 object SettingsManager {
 
@@ -69,6 +72,22 @@ object SettingsManager {
         val rulesetItems = MmkvManager.decodeRoutingRulesets()
         val exist = rulesetItems?.any { it.enabled && it.domain?.contains(":private") == true }
         return exist == true
+    }
+
+    fun swapRoutingRuleset(fromPosition: Int, toPosition: Int) {
+        val rulesetList = MmkvManager.decodeRoutingRulesets()
+        if (rulesetList.isNullOrEmpty()) return
+
+        Collections.swap(rulesetList, fromPosition, toPosition)
+        MmkvManager.encodeRoutingRulesets(rulesetList)
+    }
+
+    fun swapSubscriptions(fromPosition: Int, toPosition: Int) {
+        val subsList = MmkvManager.decodeSubsList()
+        if (subsList.isNullOrEmpty()) return
+
+        Collections.swap(subsList, fromPosition, toPosition)
+        MmkvManager.encodeSubsList(subsList)
     }
 
 

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/ui/RoutingSettingActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.AdapterView
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.appbar.CollapsingToolbarLayout
@@ -16,6 +17,7 @@ import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityRoutingSettingBinding
 import com.neko.v2ray.dto.RulesetItem
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.helper.SimpleItemTouchHelperCallback
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.MmkvManager.settingsStorage
 import com.neko.v2ray.util.SettingsManager
@@ -28,6 +30,7 @@ class RoutingSettingActivity : BaseActivity() {
 
     var rulesets: MutableList<RulesetItem> = mutableListOf()
     private val adapter by lazy { RoutingSettingRecyclerAdapter(this) }
+    private var mItemTouchHelper: ItemTouchHelper? = null
     private val routing_domain_strategy: Array<out String> by lazy {
         resources.getStringArray(R.array.routing_domain_strategy)
     }
@@ -46,6 +49,9 @@ class RoutingSettingActivity : BaseActivity() {
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
+
+        mItemTouchHelper = ItemTouchHelper(SimpleItemTouchHelperCallback(adapter))
+        mItemTouchHelper?.attachToRecyclerView(binding.recyclerView)
 
         val found = Utils.arrayFind(routing_domain_strategy, settingsStorage?.decodeString(AppConfig.PREF_ROUTING_DOMAIN_STRATEGY) ?: "")
         found.let { binding.spDomainStrategy.setSelection(if (it >= 0) it else 0) }
@@ -109,7 +115,7 @@ class RoutingSettingActivity : BaseActivity() {
         else -> super.onOptionsItemSelected(item)
     }
 
-    private fun refreshData() {
+    fun refreshData() {
         rulesets = MmkvManager.decodeRoutingRulesets() ?: mutableListOf()
         adapter.notifyDataSetChanged()
     }

app/src/main/kotlin/com/neko/v2ray/ui/RoutingSettingRecyclerAdapter.kt
@@ -3,13 +3,17 @@ package com.neko.v2ray.ui
 import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.neko.v2ray.databinding.ItemRecyclerRoutingSettingBinding
+import com.neko.v2ray.helper.ItemTouchHelperAdapter
+import com.neko.v2ray.helper.ItemTouchHelperViewHolder
+import com.neko.v2ray.ui.MainRecyclerAdapter.BaseViewHolder
 import com.neko.v2ray.util.SettingsManager
 
-class RoutingSettingRecyclerAdapter(val activity: RoutingSettingActivity) :
-    RecyclerView.Adapter<RoutingSettingRecyclerAdapter.MainViewHolder>() {
+class RoutingSettingRecyclerAdapter(val activity: RoutingSettingActivity) : RecyclerView.Adapter<RoutingSettingRecyclerAdapter.MainViewHolder>(),
+    ItemTouchHelperAdapter {
 
     private var mActivity: RoutingSettingActivity = activity
     override fun getItemCount() = mActivity.rulesets.size
@@ -47,5 +51,28 @@ class RoutingSettingRecyclerAdapter(val activity: RoutingSettingActivity) :
     }
 
     class MainViewHolder(val itemRoutingSettingBinding: ItemRecyclerRoutingSettingBinding) :
-        RecyclerView.ViewHolder(itemRoutingSettingBinding.root)
+        BaseViewHolder(itemRoutingSettingBinding.root), ItemTouchHelperViewHolder
+
+    open class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY)
+        }
+
+        fun onItemClear() {
+            itemView.setBackgroundColor(0)
+        }
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+        SettingsManager.swapRoutingRuleset(fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
+        return true
+    }
+
+    override fun onItemMoveCompleted() {
+        mActivity.refreshData()
+    }
+
+    override fun onItemDismiss(position: Int) {
+    }
 }

app/src/main/kotlin/com/neko/v2ray/ui/SubSettingActivity.kt
@@ -2,18 +2,20 @@ package com.neko.v2ray.ui
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.appbar.MaterialToolbar
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.neko.v2ray.R
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.neko.v2ray.databinding.ActivitySubSettingBinding
 import com.neko.v2ray.databinding.LayoutProgressBinding
 import com.neko.v2ray.dto.SubscriptionItem
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.helper.SimpleItemTouchHelperCallback
 import com.neko.v2ray.util.AngConfigManager
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.SoftInputAssist
@@ -27,6 +29,7 @@ class SubSettingActivity : BaseActivity() {
 
     var subscriptions: List<Pair<String, SubscriptionItem>> = listOf()
     private val adapter by lazy { SubSettingRecyclerAdapter(this) }
+    private var mItemTouchHelper: ItemTouchHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,19 +39,20 @@ class SubSettingActivity : BaseActivity() {
         val toolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        softInputAssist = SoftInputAssist(this)
 
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
-        softInputAssist = SoftInputAssist(this)
+        mItemTouchHelper = ItemTouchHelper(SimpleItemTouchHelperCallback(adapter))
+        mItemTouchHelper?.attachToRecyclerView(binding.recyclerView)
     }
 
     override fun onResume() {
         softInputAssist.onResume()
         super.onResume()
-        subscriptions = MmkvManager.decodeSubscriptions()
-        adapter.notifyDataSetChanged()
+        refreshData()
     }
 
     override fun onPause() {
@@ -96,5 +100,10 @@ class SubSettingActivity : BaseActivity() {
 
         else -> super.onOptionsItemSelected(item)
 
+    }
+
+    fun refreshData() {
+        subscriptions = MmkvManager.decodeSubscriptions()
+        adapter.notifyDataSetChanged()
     }
 }

app/src/main/kotlin/com/neko/v2ray/ui/SubSettingRecyclerAdapter.kt
@@ -8,17 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ItemQrcodeBinding
 import com.neko.v2ray.databinding.ItemRecyclerSubSettingBinding
 import com.neko.v2ray.extension.toast
-import com.neko.v2ray.util.MmkvManager.subStorage
+import com.neko.v2ray.helper.ItemTouchHelperAdapter
+import com.neko.v2ray.helper.ItemTouchHelperViewHolder
+import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.QRCodeDecoder
+import com.neko.v2ray.util.SettingsManager
 import com.neko.v2ray.util.Utils
 
-class SubSettingRecyclerAdapter(val activity: SubSettingActivity) :
-    RecyclerView.Adapter<SubSettingRecyclerAdapter.MainViewHolder>() {
+class SubSettingRecyclerAdapter(val activity: SubSettingActivity) : RecyclerView.Adapter<SubSettingRecyclerAdapter.MainViewHolder>(), ItemTouchHelperAdapter {
 
     private var mActivity: SubSettingActivity = activity
 
@@ -44,7 +45,8 @@ class SubSettingRecyclerAdapter(val activity: SubSettingActivity) :
         }
         holder.itemSubSettingBinding.chkEnable.setOnCheckedChangeListener { _, isChecked ->
             subItem.enabled = isChecked
-            subStorage?.encode(subId, Gson().toJson(subItem))
+            MmkvManager.encodeSubscription(subId, subItem)
+
         }
 
         if (TextUtils.isEmpty(subItem.url)) {
@@ -92,5 +94,28 @@ class SubSettingRecyclerAdapter(val activity: SubSettingActivity) :
     }
 
     class MainViewHolder(val itemSubSettingBinding: ItemRecyclerSubSettingBinding) :
-        RecyclerView.ViewHolder(itemSubSettingBinding.root)
+        BaseViewHolder(itemSubSettingBinding.root), ItemTouchHelperViewHolder
+
+    open class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY)
+        }
+
+        fun onItemClear() {
+            itemView.setBackgroundColor(0)
+        }
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
+        SettingsManager.swapSubscriptions(fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
+        return true
+    }
+
+    override fun onItemMoveCompleted() {
+        mActivity.refreshData()
+    }
+
+    override fun onItemDismiss(position: Int) {
+    }
 }

app/src/main/kotlin/com/neko/v2ray/util/SettingsManager.kt
@@ -2,8 +2,11 @@ package com.neko.v2ray.util
 
 import android.content.Context
 import android.text.TextUtils
+import android.util.Log
 import com.google.gson.Gson
 import com.neko.v2ray.dto.RulesetItem
+import com.neko.v2ray.util.MmkvManager.subStorage
+import java.util.Collections
 
 object SettingsManager {
 
@@ -69,6 +72,22 @@ object SettingsManager {
         val rulesetItems = MmkvManager.decodeRoutingRulesets()
         val exist = rulesetItems?.any { it.enabled && it.domain?.contains(":private") == true }
         return exist == true
+    }
+
+    fun swapRoutingRuleset(fromPosition: Int, toPosition: Int) {
+        val rulesetList = MmkvManager.decodeRoutingRulesets()
+        if (rulesetList.isNullOrEmpty()) return
+
+        Collections.swap(rulesetList, fromPosition, toPosition)
+        MmkvManager.encodeRoutingRulesets(rulesetList)
+    }
+
+    fun swapSubscriptions(fromPosition: Int, toPosition: Int) {
+        val subsList = MmkvManager.decodeSubsList()
+        if (subsList.isNullOrEmpty()) return
+
+        Collections.swap(subsList, fromPosition, toPosition)
+        MmkvManager.encodeSubsList(subsList)
     }
 
 