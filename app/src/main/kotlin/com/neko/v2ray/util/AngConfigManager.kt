2dust/v2rayNG

Add HYSTERIA2 for v2fly

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/AngConfigManager.kt
@@ -14,6 +14,7 @@ import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.dto.*
 import com.v2ray.ang.util.MmkvManager.settingsStorage
+import com.v2ray.ang.util.fmt.Hysteria2Fmt
 import com.v2ray.ang.util.fmt.ShadowsocksFmt
 import com.v2ray.ang.util.fmt.SocksFmt
 import com.v2ray.ang.util.fmt.TrojanFmt
@@ -50,6 +51,8 @@ object AngConfigManager {
                 VlessFmt.parseVless(str)
             } else if (str.startsWith(EConfigType.WIREGUARD.protocolScheme)) {
                 WireguardFmt.parseWireguard(str)
+            } else if (str.startsWith(EConfigType.HYSTERIA2.protocolScheme)) {
+                Hysteria2Fmt.parseHysteria2(str)
             } else {
                 null
             }
@@ -92,6 +95,7 @@ object AngConfigManager {
                 EConfigType.VLESS -> VlessFmt.toUri(config)
                 EConfigType.TROJAN -> TrojanFmt.toUri(config)
                 EConfigType.WIREGUARD -> WireguardFmt.toUri(config)
+                EConfigType.HYSTERIA2 -> Hysteria2Fmt.toUri(config)
             }
         } catch (e: Exception) {
             e.printStackTrace()

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/fmt/Hysteria2Fmt.kt
@@ -1,0 +1,73 @@
+package com.v2ray.ang.util.fmt
+
+import android.text.TextUtils
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.ServerConfig
+import com.v2ray.ang.dto.V2rayConfig
+import com.v2ray.ang.extension.idnHost
+import com.v2ray.ang.util.MmkvManager.settingsStorage
+import com.v2ray.ang.util.Utils
+import java.net.URI
+
+object Hysteria2Fmt {
+
+    fun parseHysteria2(str: String): ServerConfig {
+        var allowInsecure = settingsStorage?.decodeBool(AppConfig.PREF_ALLOW_INSECURE) ?: false
+        val config = ServerConfig.create(EConfigType.HYSTERIA2)
+
+        val uri = URI(Utils.fixIllegalUrl(str))
+        config.remarks = Utils.urlDecode(uri.fragment.orEmpty())
+
+        val queryParam = uri.rawQuery.split("&")
+            .associate { it.split("=").let { (k, v) -> k to Utils.urlDecode(v) } }
+
+        config.outboundBean?.streamSettings?.populateTlsSettings(
+            V2rayConfig.TLS,
+            if ((queryParam["allowInsecure"].orEmpty()) == "1") true else allowInsecure,
+            queryParam["sni"] ?: uri.idnHost,
+            null,
+            queryParam["alpn"],
+            null,
+            null,
+            null
+        )
+
+        config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+            server.address = uri.idnHost
+            server.port = uri.port
+            server.password = uri.userInfo
+        }
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+        val streamSetting = outbound.streamSettings ?: V2rayConfig.OutboundBean.StreamSettingsBean()
+
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val dicQuery = HashMap<String, String>()
+        dicQuery["security"] = streamSetting.security.ifEmpty { "none" }
+        streamSetting.tlsSettings?.let { tlsSetting ->
+            dicQuery["insecure"] = if (tlsSetting.allowInsecure) "1" else "0"
+            if (!TextUtils.isEmpty(tlsSetting.serverName)) {
+                dicQuery["sni"] = tlsSetting.serverName
+            }
+            if (!tlsSetting.alpn.isNullOrEmpty() && tlsSetting.alpn.isNotEmpty()) {
+                dicQuery["alpn"] = Utils.removeWhiteSpace(tlsSetting.alpn.joinToString()).orEmpty()
+            }
+        }
+
+        val query = "?" + dicQuery.toList().joinToString(
+            separator = "&",
+            transform = { it.first + "=" + it.second })
+
+        val url = String.format(
+            "%s@%s:%s",
+            outbound.getPassword(),
+            Utils.getIpv6Address(outbound.getServerAddress()),
+            outbound.getServerPort()
+        )
+        return url + query + remark
+    }
+}

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/util/AngConfigManager.kt
@@ -14,6 +14,7 @@ import com.neko.v2ray.AppConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.dto.*
 import com.neko.v2ray.util.MmkvManager.settingsStorage
+import com.neko.v2ray.util.fmt.Hysteria2Fmt
 import com.neko.v2ray.util.fmt.ShadowsocksFmt
 import com.neko.v2ray.util.fmt.SocksFmt
 import com.neko.v2ray.util.fmt.TrojanFmt
@@ -49,6 +50,8 @@ object AngConfigManager {
                 VlessFmt.parseVless(str)
             } else if (str.startsWith(EConfigType.WIREGUARD.protocolScheme)) {
                 WireguardFmt.parseWireguard(str)
+            } else if (str.startsWith(EConfigType.HYSTERIA2.protocolScheme)) {
+                Hysteria2Fmt.parseHysteria2(str)
             } else {
                 null
             }
@@ -91,6 +94,7 @@ object AngConfigManager {
                 EConfigType.VLESS -> VlessFmt.toUri(config)
                 EConfigType.TROJAN -> TrojanFmt.toUri(config)
                 EConfigType.WIREGUARD -> WireguardFmt.toUri(config)
+                EConfigType.HYSTERIA2 -> Hysteria2Fmt.toUri(config)
             }
         } catch (e: Exception) {
             e.printStackTrace()

app/src/main/kotlin/com/neko/v2ray/util/fmt/Hysteria2Fmt.kt
@@ -1,0 +1,73 @@
+package com.neko.v2ray.util.fmt
+
+import android.text.TextUtils
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.ServerConfig
+import com.neko.v2ray.dto.V2rayConfig
+import com.neko.v2ray.extension.idnHost
+import com.neko.v2ray.util.MmkvManager.settingsStorage
+import com.neko.v2ray.util.Utils
+import java.net.URI
+
+object Hysteria2Fmt {
+
+    fun parseHysteria2(str: String): ServerConfig {
+        var allowInsecure = settingsStorage?.decodeBool(AppConfig.PREF_ALLOW_INSECURE) ?: false
+        val config = ServerConfig.create(EConfigType.HYSTERIA2)
+
+        val uri = URI(Utils.fixIllegalUrl(str))
+        config.remarks = Utils.urlDecode(uri.fragment.orEmpty())
+
+        val queryParam = uri.rawQuery.split("&")
+            .associate { it.split("=").let { (k, v) -> k to Utils.urlDecode(v) } }
+
+        config.outboundBean?.streamSettings?.populateTlsSettings(
+            V2rayConfig.TLS,
+            if ((queryParam["allowInsecure"].orEmpty()) == "1") true else allowInsecure,
+            queryParam["sni"] ?: uri.idnHost,
+            null,
+            queryParam["alpn"],
+            null,
+            null,
+            null
+        )
+
+        config.outboundBean?.settings?.servers?.get(0)?.let { server ->
+            server.address = uri.idnHost
+            server.port = uri.port
+            server.password = uri.userInfo
+        }
+        return config
+    }
+
+    fun toUri(config: ServerConfig): String {
+        val outbound = config.getProxyOutbound() ?: return ""
+        val streamSetting = outbound.streamSettings ?: V2rayConfig.OutboundBean.StreamSettingsBean()
+
+        val remark = "#" + Utils.urlEncode(config.remarks)
+        val dicQuery = HashMap<String, String>()
+        dicQuery["security"] = streamSetting.security.ifEmpty { "none" }
+        streamSetting.tlsSettings?.let { tlsSetting ->
+            dicQuery["insecure"] = if (tlsSetting.allowInsecure) "1" else "0"
+            if (!TextUtils.isEmpty(tlsSetting.serverName)) {
+                dicQuery["sni"] = tlsSetting.serverName
+            }
+            if (!tlsSetting.alpn.isNullOrEmpty() && tlsSetting.alpn.isNotEmpty()) {
+                dicQuery["alpn"] = Utils.removeWhiteSpace(tlsSetting.alpn.joinToString()).orEmpty()
+            }
+        }
+
+        val query = "?" + dicQuery.toList().joinToString(
+            separator = "&",
+            transform = { it.first + "=" + it.second })
+
+        val url = String.format(
+            "%s@%s:%s",
+            outbound.getPassword(),
+            Utils.getIpv6Address(outbound.getServerAddress()),
+            outbound.getServerPort()
+        )
+        return url + query + remark
+    }
+}