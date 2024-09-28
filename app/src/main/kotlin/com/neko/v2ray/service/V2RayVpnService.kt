2dust/v2rayNG

Subscribe to add alias regular filter Add const LOOPBACK

V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayVpnService.kt
@@ -17,6 +17,7 @@ import android.os.StrictMode
 import android.util.Log
 import androidx.annotation.RequiresApi
 import com.v2ray.ang.AppConfig
+import com.v2ray.ang.AppConfig.LOOPBACK
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.util.MmkvManager.settingsStorage
@@ -207,7 +208,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
             File(applicationContext.applicationInfo.nativeLibraryDir, TUN2SOCKS).absolutePath,
             "--netif-ipaddr", PRIVATE_VLAN4_ROUTER,
             "--netif-netmask", "255.255.255.252",
-            "--socks-server-addr", "127.0.0.1:${socksPort}",
+            "--socks-server-addr", "$LOOPBACK:${socksPort}",
             "--tunmtu", VPN_MTU.toString(),
             "--sock-path", "sock_path",//File(applicationContext.filesDir, "sock_path").absolutePath,
             "--enable-udprelay",
@@ -221,7 +222,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
         if (settingsStorage?.decodeBool(AppConfig.PREF_LOCAL_DNS_ENABLED) == true) {
             val localDnsPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_LOCAL_DNS_PORT), AppConfig.PORT_LOCAL_DNS.toInt())
             cmd.add("--dnsgw")
-            cmd.add("127.0.0.1:${localDnsPort}")
+            cmd.add("$LOOPBACK:${localDnsPort}")
         }
         Log.d(packageName, cmd.toString())
 

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/UserAssetActivity.kt
@@ -21,6 +21,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tbruyelle.rxpermissions3.RxPermissions
 import com.v2ray.ang.AppConfig
+import com.v2ray.ang.AppConfig.LOOPBACK
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivitySubSettingBinding
 import com.v2ray.ang.databinding.ItemRecyclerUserAssetBinding
@@ -212,7 +213,7 @@ class UserAssetActivity : BaseActivity() {
                 URL(item.url).openConnection(
                     Proxy(
                         Proxy.Type.HTTP,
-                        InetSocketAddress("127.0.0.1", httpPort)
+                        InetSocketAddress(LOOPBACK, httpPort)
                     )
                 ) as HttpURLConnection
             }

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/SpeedtestUtil.kt
@@ -5,6 +5,7 @@ import android.os.SystemClock
 import android.text.TextUtils
 import android.util.Log
 import com.v2ray.ang.AppConfig
+import com.v2ray.ang.AppConfig.LOOPBACK
 import com.v2ray.ang.R
 import com.v2ray.ang.extension.responseLength
 import kotlinx.coroutines.isActive
@@ -108,7 +109,7 @@ object SpeedtestUtil {
             conn = url.openConnection(
                 Proxy(
                     Proxy.Type.HTTP,
-                    InetSocketAddress("127.0.0.1", port)
+                    InetSocketAddress(LOOPBACK, port)
                 )
             ) as HttpURLConnection
             conn.connectTimeout = 30000

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/Utils.kt
@@ -19,6 +19,7 @@ import android.webkit.URLUtil
 import androidx.appcompat.app.AppCompatDelegate
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.AppConfig.ANG_PACKAGE
+import com.v2ray.ang.AppConfig.LOOPBACK
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.extension.toast
@@ -359,7 +360,7 @@ object Utils {
             url.openConnection(
                 Proxy(
                     Proxy.Type.HTTP,
-                    InetSocketAddress("127.0.0.1", httpPort)
+                    InetSocketAddress(LOOPBACK, httpPort)
                 )
             )
         }

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/service/V2RayVpnService.kt
@@ -17,6 +17,7 @@ import android.os.StrictMode
 import android.util.Log
 import androidx.annotation.RequiresApi
 import com.neko.v2ray.AppConfig
+import com.neko.v2ray.AppConfig.LOOPBACK
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.util.MmkvManager.settingsStorage
@@ -207,7 +208,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
             File(applicationContext.applicationInfo.nativeLibraryDir, TUN2SOCKS).absolutePath,
             "--netif-ipaddr", PRIVATE_VLAN4_ROUTER,
             "--netif-netmask", "255.255.255.252",
-            "--socks-server-addr", "127.0.0.1:${socksPort}",
+            "--socks-server-addr", "$LOOPBACK:${socksPort}",
             "--tunmtu", VPN_MTU.toString(),
             "--sock-path", "sock_path",//File(applicationContext.filesDir, "sock_path").absolutePath,
             "--enable-udprelay",
@@ -221,7 +222,7 @@ class V2RayVpnService : VpnService(), ServiceControl {
         if (settingsStorage?.decodeBool(AppConfig.PREF_LOCAL_DNS_ENABLED) == true) {
             val localDnsPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_LOCAL_DNS_PORT), AppConfig.PORT_LOCAL_DNS.toInt())
             cmd.add("--dnsgw")
-            cmd.add("127.0.0.1:${localDnsPort}")
+            cmd.add("$LOOPBACK:${localDnsPort}")
         }
         Log.d(packageName, cmd.toString())
 

app/src/main/kotlin/com/neko/v2ray/ui/UserAssetActivity.kt
@@ -23,8 +23,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tbruyelle.rxpermissions3.RxPermissions
 import com.neko.v2ray.AppConfig
+import com.neko.v2ray.AppConfig.LOOPBACK
 import com.neko.v2ray.R
-import com.neko.v2ray.databinding.ActivityUserAssetBinding
+import com.neko.v2ray.databinding.ActivitySubSettingBinding
 import com.neko.v2ray.databinding.ItemRecyclerUserAssetBinding
 import com.neko.v2ray.databinding.LayoutProgressBinding
 import com.neko.v2ray.dto.AssetUrlItem
@@ -218,7 +219,7 @@ class UserAssetActivity : BaseActivity() {
                 URL(item.url).openConnection(
                     Proxy(
                         Proxy.Type.HTTP,
-                        InetSocketAddress("127.0.0.1", httpPort)
+                        InetSocketAddress(LOOPBACK, httpPort)
                     )
                 ) as HttpURLConnection
             }

app/src/main/kotlin/com/neko/v2ray/util/SpeedtestUtil.kt
@@ -5,6 +5,7 @@ import android.os.SystemClock
 import android.text.TextUtils
 import android.util.Log
 import com.neko.v2ray.AppConfig
+import com.neko.v2ray.AppConfig.LOOPBACK
 import com.neko.v2ray.R
 import com.neko.v2ray.extension.responseLength
 import kotlinx.coroutines.isActive
@@ -108,7 +109,7 @@ object SpeedtestUtil {
             conn = url.openConnection(
                 Proxy(
                     Proxy.Type.HTTP,
-                    InetSocketAddress("127.0.0.1", port)
+                    InetSocketAddress(LOOPBACK, port)
                 )
             ) as HttpURLConnection
             conn.connectTimeout = 30000

app/src/main/kotlin/com/neko/v2ray/util/Utils.kt
@@ -19,6 +19,7 @@ import android.webkit.URLUtil
 import androidx.appcompat.app.AppCompatDelegate
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.AppConfig.ANG_PACKAGE
+import com.neko.v2ray.AppConfig.LOOPBACK
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.extension.toast
@@ -359,7 +360,7 @@ object Utils {
             url.openConnection(
                 Proxy(
                     Proxy.Type.HTTP,
-                    InetSocketAddress("127.0.0.1", httpPort)
+                    InetSocketAddress(LOOPBACK, httpPort)
                 )
             )
         }