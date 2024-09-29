2dust/v2rayNG

Run hysteria using a plugin-like method

V2rayNG/app/src/main/kotlin/com/v2ray/ang/plugin/NativePlugin.kt
@@ -1,0 +1,32 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ * Copyright (C) 2021 by Max Lv <max.c.lv@gmail.com>                          *
+ * Copyright (C) 2021 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.v2ray.ang.plugin
+
+import android.content.pm.ResolveInfo
+
+class NativePlugin(resolveInfo: ResolveInfo) : ResolvedPlugin(resolveInfo) {
+    init {
+        check(resolveInfo.providerInfo != null)
+    }
+
+    override val componentInfo get() = resolveInfo.providerInfo!!
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/plugin/PluginContract.kt
@@ -1,0 +1,33 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.v2ray.ang.plugin
+
+object PluginContract {
+
+    const val ACTION_NATIVE_PLUGIN = "io.nekohasekai.sagernet.plugin.ACTION_NATIVE_PLUGIN"
+    const val EXTRA_ENTRY = "io.nekohasekai.sagernet.plugin.EXTRA_ENTRY"
+    const val METADATA_KEY_ID = "io.nekohasekai.sagernet.plugin.id"
+    const val METADATA_KEY_EXECUTABLE_PATH = "io.nekohasekai.sagernet.plugin.executable_path"
+    const val METHOD_GET_EXECUTABLE = "sagernet:getExecutable"
+
+    const val COLUMN_PATH = "path"
+    const val COLUMN_MODE = "mode"
+    const val SCHEME = "plugin"
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/plugin/PluginList.kt
@@ -1,0 +1,53 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ * Copyright (C) 2021 by Max Lv <max.c.lv@gmail.com>                          *
+ * Copyright (C) 2021 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.v2ray.ang.plugin
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import com.v2ray.ang.AngApplication
+
+class PluginList : ArrayList<Plugin>() {
+    init {
+        addAll(
+            AngApplication.application.packageManager.queryIntentContentProviders(
+            Intent(PluginContract.ACTION_NATIVE_PLUGIN), PackageManager.GET_META_DATA)
+            .filter { it.providerInfo.exported }.map { NativePlugin(it) })
+    }
+
+    val lookup = mutableMapOf<String, Plugin>().apply {
+        for (plugin in this@PluginList.toList()) {
+            fun check(old: Plugin?) {
+                if (old != null && old != plugin) {
+                    this@PluginList.remove(old)
+                }
+               /* if (old != null && old !== plugin) {
+                    val packages = this@PluginList.filter { it.id == plugin.id }
+                        .joinToString { it.packageName }
+                    val message = "Conflicting plugins found from: $packages"
+                    Toast.makeText(SagerNet.application, message, Toast.LENGTH_LONG).show()
+                    throw IllegalStateException(message)
+                }*/
+            }
+            check(put(plugin.id, plugin))
+        }
+    }
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/plugin/ResolvedPlugin.kt
@@ -1,0 +1,45 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ * Copyright (C) 2021 by Max Lv <max.c.lv@gmail.com>                          *
+ * Copyright (C) 2021 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.v2ray.ang.plugin
+
+import android.content.pm.ComponentInfo
+import android.content.pm.ResolveInfo
+import android.graphics.drawable.Drawable
+import android.os.Build
+import com.v2ray.ang.AngApplication
+import com.v2ray.ang.plugin.PluginManager.loadString
+
+abstract class ResolvedPlugin(protected val resolveInfo: ResolveInfo) : Plugin() {
+    protected abstract val componentInfo: ComponentInfo
+
+    override val id by lazy { componentInfo.loadString(PluginContract.METADATA_KEY_ID)!! }
+    override val version by lazy {
+        AngApplication.application.getPackageInfo(componentInfo.packageName).versionCode
+    }
+    override val versionName: String by lazy {
+        AngApplication.application.getPackageInfo(componentInfo.packageName).versionName!!
+    }
+    override val label: CharSequence get() = resolveInfo.loadLabel(AngApplication.application.packageManager)
+    override val icon: Drawable get() = resolveInfo.loadIcon(AngApplication.application.packageManager)
+    override val packageName: String get() = componentInfo.packageName
+    override val directBootAware get() = Build.VERSION.SDK_INT < 24 || componentInfo.directBootAware
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayServiceManager.kt
@@ -24,6 +24,7 @@ import com.v2ray.ang.ui.MainActivity
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.MmkvManager.settingsStorage
+import com.v2ray.ang.util.PluginUtil
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.util.V2rayConfigUtil
 import go.Seq
@@ -161,6 +162,8 @@ object V2RayServiceManager {
         if (v2rayPoint.isRunning) {
             MessageUtil.sendMsg2UI(service, AppConfig.MSG_STATE_START_SUCCESS, "")
             showNotification()
+
+            PluginUtil.runPlugin(service, config)
         } else {
             MessageUtil.sendMsg2UI(service, AppConfig.MSG_STATE_START_FAILURE, "")
             cancelNotification()
@@ -188,6 +191,7 @@ object V2RayServiceManager {
         } catch (e: Exception) {
             Log.d(ANG_PACKAGE, e.toString())
         }
+        PluginUtil.stopPlugin()
     }
 
     private class ReceiveMessageHandler : BroadcastReceiver() {

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/PluginUtil.kt
@@ -1,0 +1,93 @@
+package com.v2ray.ang.util
+
+import android.content.Context
+import android.os.SystemClock
+import android.util.Log
+import com.google.gson.Gson
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.ServerConfig
+import com.v2ray.ang.util.MmkvManager.settingsStorage
+import com.v2ray.ang.util.fmt.Hysteria2Fmt
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+
+object PluginUtil {
+    //private const val HYSTERIA2 = "hysteria2-plugin"
+    private const val HYSTERIA2 = "libhysteria2.so"
+    private const val packageName = "PluginUtil"
+    private lateinit var process: Process
+
+//    fun initPlugin(name: String): PluginManager.InitResult {
+//        return PluginManager.init(name)!!
+//    }
+
+    fun runPlugin(context: Context, config: ServerConfig?) {
+        Log.d(packageName, "runPlugin")
+
+        val outbound = config?.getProxyOutbound() ?: return
+        if (outbound.protocol.equals(EConfigType.HYSTERIA2.name, true)) {
+            Log.d(packageName, "runPlugin $HYSTERIA2")
+
+            val socksPort = 100 + Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
+            val hy2Config = Hysteria2Fmt.toNativeConfig(config, socksPort) ?: return
+
+            val configFile = File(context.noBackupFilesDir, "hy2_${SystemClock.elapsedRealtime()}.json")
+            Log.d(packageName, "runPlugin ${configFile.absolutePath}")
+
+            configFile.parentFile?.mkdirs()
+            configFile.writeText(Gson().toJson(hy2Config))
+            Log.d(packageName, Gson().toJson(hy2Config))
+
+            runHy2(context, configFile)
+        }
+    }
+
+    fun stopPlugin() {
+        stopHy2()
+    }
+
+    private fun runHy2(context: Context, configFile: File) {
+        val cmd = mutableListOf(
+            File(context.applicationInfo.nativeLibraryDir, HYSTERIA2).absolutePath,
+            //initPlugin(HYSTERIA2).path,
+            "--disable-update-check",
+            "--config",
+            configFile.absolutePath,
+            "--log-level",
+            "warn",
+            "client"
+        )
+        Log.d(packageName, cmd.toString())
+
+        try {
+            val proBuilder = ProcessBuilder(cmd)
+            proBuilder.redirectErrorStream(true)
+            process = proBuilder
+                .directory(context.filesDir)
+                .start()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                Thread.sleep(500L)
+                Log.d(packageName, "$HYSTERIA2 check")
+                process.waitFor()
+                Log.d(packageName, "$HYSTERIA2 exited")
+            }
+            Log.d(packageName, process.toString())
+
+        } catch (e: Exception) {
+            Log.d(packageName, e.toString())
+        }
+    }
+
+    private fun stopHy2() {
+        try {
+            Log.d(packageName, "$HYSTERIA2 destroy")
+            process?.destroy()
+        } catch (e: Exception) {
+            Log.d(packageName, e.toString())
+        }
+    }
+}

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/plugin/NativePlugin.kt
@@ -1,0 +1,32 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ * Copyright (C) 2021 by Max Lv <max.c.lv@gmail.com>                          *
+ * Copyright (C) 2021 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.neko.v2ray.plugin
+
+import android.content.pm.ResolveInfo
+
+class NativePlugin(resolveInfo: ResolveInfo) : ResolvedPlugin(resolveInfo) {
+    init {
+        check(resolveInfo.providerInfo != null)
+    }
+
+    override val componentInfo get() = resolveInfo.providerInfo!!
+}

app/src/main/kotlin/com/neko/v2ray/plugin/PluginContract.kt
@@ -1,0 +1,33 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.neko.v2ray.plugin
+
+object PluginContract {
+
+    const val ACTION_NATIVE_PLUGIN = "io.nekohasekai.sagernet.plugin.ACTION_NATIVE_PLUGIN"
+    const val EXTRA_ENTRY = "io.nekohasekai.sagernet.plugin.EXTRA_ENTRY"
+    const val METADATA_KEY_ID = "io.nekohasekai.sagernet.plugin.id"
+    const val METADATA_KEY_EXECUTABLE_PATH = "io.nekohasekai.sagernet.plugin.executable_path"
+    const val METHOD_GET_EXECUTABLE = "sagernet:getExecutable"
+
+    const val COLUMN_PATH = "path"
+    const val COLUMN_MODE = "mode"
+    const val SCHEME = "plugin"
+}

app/src/main/kotlin/com/neko/v2ray/plugin/PluginList.kt
@@ -1,0 +1,53 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ * Copyright (C) 2021 by Max Lv <max.c.lv@gmail.com>                          *
+ * Copyright (C) 2021 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.neko.v2ray.plugin
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import com.neko.v2ray.AngApplication
+
+class PluginList : ArrayList<Plugin>() {
+    init {
+        addAll(
+            AngApplication.application.packageManager.queryIntentContentProviders(
+            Intent(PluginContract.ACTION_NATIVE_PLUGIN), PackageManager.GET_META_DATA)
+            .filter { it.providerInfo.exported }.map { NativePlugin(it) })
+    }
+
+    val lookup = mutableMapOf<String, Plugin>().apply {
+        for (plugin in this@PluginList.toList()) {
+            fun check(old: Plugin?) {
+                if (old != null && old != plugin) {
+                    this@PluginList.remove(old)
+                }
+               /* if (old != null && old !== plugin) {
+                    val packages = this@PluginList.filter { it.id == plugin.id }
+                        .joinToString { it.packageName }
+                    val message = "Conflicting plugins found from: $packages"
+                    Toast.makeText(SagerNet.application, message, Toast.LENGTH_LONG).show()
+                    throw IllegalStateException(message)
+                }*/
+            }
+            check(put(plugin.id, plugin))
+        }
+    }
+}

app/src/main/kotlin/com/neko/v2ray/plugin/ResolvedPlugin.kt
@@ -1,0 +1,45 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2021 by nekohasekai <contact-sagernet@sekai.icu>             *
+ * Copyright (C) 2021 by Max Lv <max.c.lv@gmail.com>                          *
+ * Copyright (C) 2021 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.neko.v2ray.plugin
+
+import android.content.pm.ComponentInfo
+import android.content.pm.ResolveInfo
+import android.graphics.drawable.Drawable
+import android.os.Build
+import com.neko.v2ray.AngApplication
+import com.neko.v2ray.plugin.PluginManager.loadString
+
+abstract class ResolvedPlugin(protected val resolveInfo: ResolveInfo) : Plugin() {
+    protected abstract val componentInfo: ComponentInfo
+
+    override val id by lazy { componentInfo.loadString(PluginContract.METADATA_KEY_ID)!! }
+    override val version by lazy {
+        AngApplication.application.getPackageInfo(componentInfo.packageName).versionCode
+    }
+    override val versionName: String by lazy {
+        AngApplication.application.getPackageInfo(componentInfo.packageName).versionName!!
+    }
+    override val label: CharSequence get() = resolveInfo.loadLabel(AngApplication.application.packageManager)
+    override val icon: Drawable get() = resolveInfo.loadIcon(AngApplication.application.packageManager)
+    override val packageName: String get() = componentInfo.packageName
+    override val directBootAware get() = Build.VERSION.SDK_INT < 24 || componentInfo.directBootAware
+}

app/src/main/kotlin/com/neko/v2ray/service/V2RayServiceManager.kt
@@ -24,6 +24,7 @@ import com.neko.v2ray.ui.MainActivity
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.MmkvManager.settingsStorage
+import com.neko.v2ray.util.PluginUtil
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.util.V2rayConfigUtil
 import go.Seq
@@ -161,6 +162,8 @@ object V2RayServiceManager {
         if (v2rayPoint.isRunning) {
             MessageUtil.sendMsg2UI(service, AppConfig.MSG_STATE_START_SUCCESS, "")
             showNotification()
+
+            PluginUtil.runPlugin(service, config)
         } else {
             MessageUtil.sendMsg2UI(service, AppConfig.MSG_STATE_START_FAILURE, "")
             cancelNotification()
@@ -188,6 +191,7 @@ object V2RayServiceManager {
         } catch (e: Exception) {
             Log.d(ANG_PACKAGE, e.toString())
         }
+        PluginUtil.stopPlugin()
     }
 
     private class ReceiveMessageHandler : BroadcastReceiver() {

app/src/main/kotlin/com/neko/v2ray/util/PluginUtil.kt
@@ -1,0 +1,93 @@
+package com.neko.v2ray.util
+
+import android.content.Context
+import android.os.SystemClock
+import android.util.Log
+import com.google.gson.Gson
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.ServerConfig
+import com.neko.v2ray.util.MmkvManager.settingsStorage
+import com.neko.v2ray.util.fmt.Hysteria2Fmt
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+
+object PluginUtil {
+    //private const val HYSTERIA2 = "hysteria2-plugin"
+    private const val HYSTERIA2 = "libhysteria2.so"
+    private const val packageName = "PluginUtil"
+    private lateinit var process: Process
+
+//    fun initPlugin(name: String): PluginManager.InitResult {
+//        return PluginManager.init(name)!!
+//    }
+
+    fun runPlugin(context: Context, config: ServerConfig?) {
+        Log.d(packageName, "runPlugin")
+
+        val outbound = config?.getProxyOutbound() ?: return
+        if (outbound.protocol.equals(EConfigType.HYSTERIA2.name, true)) {
+            Log.d(packageName, "runPlugin $HYSTERIA2")
+
+            val socksPort = 100 + Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
+            val hy2Config = Hysteria2Fmt.toNativeConfig(config, socksPort) ?: return
+
+            val configFile = File(context.noBackupFilesDir, "hy2_${SystemClock.elapsedRealtime()}.json")
+            Log.d(packageName, "runPlugin ${configFile.absolutePath}")
+
+            configFile.parentFile?.mkdirs()
+            configFile.writeText(Gson().toJson(hy2Config))
+            Log.d(packageName, Gson().toJson(hy2Config))
+
+            runHy2(context, configFile)
+        }
+    }
+
+    fun stopPlugin() {
+        stopHy2()
+    }
+
+    private fun runHy2(context: Context, configFile: File) {
+        val cmd = mutableListOf(
+            File(context.applicationInfo.nativeLibraryDir, HYSTERIA2).absolutePath,
+            //initPlugin(HYSTERIA2).path,
+            "--disable-update-check",
+            "--config",
+            configFile.absolutePath,
+            "--log-level",
+            "warn",
+            "client"
+        )
+        Log.d(packageName, cmd.toString())
+
+        try {
+            val proBuilder = ProcessBuilder(cmd)
+            proBuilder.redirectErrorStream(true)
+            process = proBuilder
+                .directory(context.filesDir)
+                .start()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                Thread.sleep(500L)
+                Log.d(packageName, "$HYSTERIA2 check")
+                process.waitFor()
+                Log.d(packageName, "$HYSTERIA2 exited")
+            }
+            Log.d(packageName, process.toString())
+
+        } catch (e: Exception) {
+            Log.d(packageName, e.toString())
+        }
+    }
+
+    private fun stopHy2() {
+        try {
+            Log.d(packageName, "$HYSTERIA2 destroy")
+            process?.destroy()
+        } catch (e: Exception) {
+            Log.d(packageName, e.toString())
+        }
+    }
+}