2dust/v2rayNG

Update subscriptions through the proxy first, then update them directly
https://github.com/2dust/v2rayNG/issues/3627

V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/SubscriptionUpdater.kt
@@ -15,6 +15,7 @@ import com.v2ray.ang.AppConfig.SUBSCRIPTION_UPDATE_CHANNEL
 import com.v2ray.ang.AppConfig.SUBSCRIPTION_UPDATE_CHANNEL_NAME
 import com.v2ray.ang.R
 import com.v2ray.ang.util.AngConfigManager
+import com.v2ray.ang.util.AngConfigManager.updateConfigViaSub
 import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.Utils
 
@@ -40,8 +41,8 @@ object SubscriptionUpdater {
 
             val subs = MmkvManager.decodeSubscriptions().filter { it.second.autoUpdate }
 
-            for (i in subs) {
-                val subscription = i.second
+            for (sub in subs) {
+                val subItem = sub.second
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     notification.setChannelId(SUBSCRIPTION_UPDATE_CHANNEL)
@@ -56,11 +57,10 @@ object SubscriptionUpdater {
                 notificationManager.notify(3, notification.build())
                 Log.d(
                     AppConfig.ANG_PACKAGE,
-                    "subscription automatic update: ---${subscription.remarks}"
+                    "subscription automatic update: ---${subItem.remarks}"
                 )
-                val configs = Utils.getUrlContentWithCustomUserAgent(subscription.url)
-                AngConfigManager.importBatchConfig(configs, i.first, false)
-                notification.setContentText("Updating ${subscription.remarks}")
+                updateConfigViaSub(Pair(sub.first, subItem))
+                notification.setContentText("Updating ${subItem.remarks}")
             }
             notificationManager.cancel(3)
             return Result.success()

V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/UserAssetActivity.kt
@@ -31,6 +31,7 @@ import com.v2ray.ang.extension.toTrafficString
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.util.MmkvManager
 import com.v2ray.ang.util.MmkvManager.settingsStorage
+import com.v2ray.ang.util.SettingsManager
 import com.v2ray.ang.util.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -176,7 +177,7 @@ class UserAssetActivity : BaseActivity() {
             .show()
         toast(R.string.msg_downloading_content)
 
-        val httpPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT), AppConfig.PORT_HTTP.toInt())
+        val httpPort = SettingsManager.getHttpPort()
         var assets = MmkvManager.decodeAssetUrls()
         assets = addBuiltInGeoItems(assets)
 

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/SettingsManager.kt
@@ -3,11 +3,14 @@ package com.v2ray.ang.util
 import android.content.Context
 import android.text.TextUtils
 import com.google.gson.Gson
+import com.v2ray.ang.AppConfig
 import com.v2ray.ang.dto.RulesetItem
 import com.v2ray.ang.dto.ServerConfig
 import com.v2ray.ang.util.MmkvManager.decodeProfileConfig
 import com.v2ray.ang.util.MmkvManager.decodeServerConfig
 import com.v2ray.ang.util.MmkvManager.decodeServerList
+import com.v2ray.ang.util.MmkvManager.settingsStorage
+import com.v2ray.ang.util.Utils.parseInt
 import java.util.Collections
 
 object SettingsManager {
@@ -138,6 +141,14 @@ object SettingsManager {
             }
         }
         return null
+    }
+
+    fun getSocksPort(): Int {
+        return parseInt(settingsStorage?.decodeString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
+    }
+
+    fun getHttpPort(): Int {
+        return parseInt(settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT), AppConfig.PORT_HTTP.toInt())
     }
 
 }

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/service/SubscriptionUpdater.kt
@@ -15,6 +15,7 @@ import com.neko.v2ray.AppConfig.SUBSCRIPTION_UPDATE_CHANNEL
 import com.neko.v2ray.AppConfig.SUBSCRIPTION_UPDATE_CHANNEL_NAME
 import com.neko.v2ray.R
 import com.neko.v2ray.util.AngConfigManager
+import com.neko.v2ray.util.AngConfigManager.updateConfigViaSub
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.Utils
 
@@ -40,8 +41,8 @@ object SubscriptionUpdater {
 
             val subs = MmkvManager.decodeSubscriptions().filter { it.second.autoUpdate }
 
-            for (i in subs) {
-                val subscription = i.second
+            for (sub in subs) {
+                val subItem = sub.second
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     notification.setChannelId(SUBSCRIPTION_UPDATE_CHANNEL)
@@ -56,11 +57,10 @@ object SubscriptionUpdater {
                 notificationManager.notify(3, notification.build())
                 Log.d(
                     AppConfig.ANG_PACKAGE,
-                    "subscription automatic update: ---${subscription.remarks}"
+                    "subscription automatic update: ---${subItem.remarks}"
                 )
-                val configs = Utils.getUrlContentWithCustomUserAgent(subscription.url)
-                AngConfigManager.importBatchConfig(configs, i.first, false)
-                notification.setContentText("Updating ${subscription.remarks}")
+                updateConfigViaSub(Pair(sub.first, subItem))
+                notification.setContentText("Updating ${subItem.remarks}")
             }
             notificationManager.cancel(3)
             return Result.success()

app/src/main/kotlin/com/neko/v2ray/ui/UserAssetActivity.kt
@@ -33,6 +33,7 @@ import com.neko.v2ray.extension.toTrafficString
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.util.MmkvManager
 import com.neko.v2ray.util.MmkvManager.settingsStorage
+import com.neko.v2ray.util.SettingsManager
 import com.neko.v2ray.util.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -182,7 +183,7 @@ class UserAssetActivity : BaseActivity() {
             .show()
         toast(R.string.msg_downloading_content)
 
-        val httpPort = Utils.parseInt(settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT), AppConfig.PORT_HTTP.toInt())
+        val httpPort = SettingsManager.getHttpPort()
         var assets = MmkvManager.decodeAssetUrls()
         assets = addBuiltInGeoItems(assets)
 

app/src/main/kotlin/com/neko/v2ray/util/SettingsManager.kt
@@ -2,12 +2,15 @@ package com.neko.v2ray.util
 
 import android.content.Context
 import android.text.TextUtils
-import com.google.gson.Gson
+import com.google.gson.Gsonl
+import com.neko.v2ray.AppConfig
 import com.neko.v2ray.dto.RulesetItem
 import com.neko.v2ray.dto.ServerConfig
 import com.neko.v2ray.util.MmkvManager.decodeProfileConfig
 import com.neko.v2ray.util.MmkvManager.decodeServerConfig
 import com.neko.v2ray.util.MmkvManager.decodeServerList
+import com.neko.v2ray.util.MmkvManager.settingsStorage
+import com.neko.v2ray.util.Utils.parseInt
 import java.util.Collections
 
 object SettingsManager {
@@ -138,6 +141,14 @@ object SettingsManager {
             }
         }
         return null
+    }
+
+    fun getSocksPort(): Int {
+        return parseInt(settingsStorage?.decodeString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
+    }
+
+    fun getHttpPort(): Int {
+        return parseInt(settingsStorage?.decodeString(AppConfig.PREF_HTTP_PORT), AppConfig.PORT_HTTP.toInt())
     }
 
 }