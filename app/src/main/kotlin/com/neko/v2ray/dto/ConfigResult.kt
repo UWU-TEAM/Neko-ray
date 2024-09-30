2dust/v2rayNG

Improvement Intent.serializable

V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/ConfigResult.kt
@@ -1,0 +1,9 @@
+package com.v2ray.ang.dto
+
+data class ConfigResult (
+    var status: Boolean,
+    var guid: String? = null,
+    var content: String = "",
+    var domainPort: String? = null,
+)
+

V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayTestService.kt
@@ -3,9 +3,12 @@ package com.v2ray.ang.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.google.gson.Gson
 import com.v2ray.ang.AppConfig.MSG_MEASURE_CONFIG
 import com.v2ray.ang.AppConfig.MSG_MEASURE_CONFIG_CANCEL
 import com.v2ray.ang.AppConfig.MSG_MEASURE_CONFIG_SUCCESS
+import com.v2ray.ang.dto.ConfigResult
+import com.v2ray.ang.extension.serializable
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.SpeedtestUtil
 import com.v2ray.ang.util.Utils
@@ -30,10 +33,11 @@ class V2RayTestService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when (intent?.getIntExtra("key", 0)) {
             MSG_MEASURE_CONFIG -> {
-                val contentPair = intent.getSerializableExtra("content") as Pair<String, String>
+                val content = intent.serializable<String>("content")
+                val config = Gson().fromJson(content, ConfigResult::class.java)
                 realTestScope.launch {
-                    val result = SpeedtestUtil.realPing(contentPair.second)
-                    MessageUtil.sendMsg2UI(this@V2RayTestService, MSG_MEASURE_CONFIG_SUCCESS, Pair(contentPair.first, result))
+                    val result = SpeedtestUtil.realPing(config.content)
+                    MessageUtil.sendMsg2UI(this@V2RayTestService, MSG_MEASURE_CONFIG_SUCCESS, Pair(config.guid, result))
                 }
             }
 

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/V2rayConfigUtil.kt
@@ -14,6 +14,7 @@ import com.v2ray.ang.AppConfig.TAG_FRAGMENT
 import com.v2ray.ang.AppConfig.TAG_PROXY
 import com.v2ray.ang.AppConfig.WIREGUARD_LOCAL_ADDRESS_V4
 import com.v2ray.ang.AppConfig.WIREGUARD_LOCAL_ADDRESS_V6
+import com.v2ray.ang.dto.ConfigResult
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.RulesetItem
 import com.v2ray.ang.dto.ServerConfig
@@ -25,34 +26,32 @@ import com.v2ray.ang.util.MmkvManager.settingsStorage
 
 object V2rayConfigUtil {
 
-    data class Result(var status: Boolean, var content: String = "", var domainPort: String? = null)
-
-    fun getV2rayConfig(context: Context, guid: String): Result {
+    fun getV2rayConfig(context: Context, guid: String): ConfigResult {
         try {
-            val config = MmkvManager.decodeServerConfig(guid) ?: return Result(false)
+            val config = MmkvManager.decodeServerConfig(guid) ?: return ConfigResult(false)
             if (config.configType == EConfigType.CUSTOM) {
                 val raw = MmkvManager.decodeServerRaw(guid)
                 val customConfig = if (raw.isNullOrBlank()) {
-                    config.fullConfig?.toPrettyPrinting() ?: return Result(false)
+                    config.fullConfig?.toPrettyPrinting() ?: return ConfigResult(false)
                 } else {
                     raw
                 }
                 val domainPort = config.getProxyOutbound()?.getServerAddressAndPort()
-                return Result(true, customConfig, domainPort)
+                return ConfigResult(true, guid, customConfig, domainPort)
             }
 
             val result = getV2rayNonCustomConfig(context, config)
             //Log.d(ANG_PACKAGE, result.content)
-            Log.d(ANG_PACKAGE, result.domainPort ?: "")
+            result.guid = guid
             return result
         } catch (e: Exception) {
             e.printStackTrace()
-            return Result(false)
+            return ConfigResult(false)
         }
     }
 
-    private fun getV2rayNonCustomConfig(context: Context, config: ServerConfig): Result {
-        val result = Result(false)
+    private fun getV2rayNonCustomConfig(context: Context, config: ServerConfig): ConfigResult {
+        val result = ConfigResult(false)
 
         val outbound = config.getProxyOutbound() ?: return result
         val address = outbound.getServerAddress() ?: return result

V2rayNG/app/src/main/kotlin/com/v2ray/ang/viewmodel/MainViewModel.kt
@@ -21,6 +21,7 @@ import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.ServerConfig
 import com.v2ray.ang.dto.ServersCache
 import com.v2ray.ang.dto.V2rayConfig
+import com.v2ray.ang.extension.serializable
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.util.AngConfigManager
 import com.v2ray.ang.util.AngConfigManager.updateConfigViaSub
@@ -216,7 +217,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                     MessageUtil.sendMsg2TestService(
                         getApplication(),
                         AppConfig.MSG_MEASURE_CONFIG,
-                        Pair(item.guid, config.content)
+                        Gson().toJson(config)
                     )
                 }
             }
@@ -394,11 +395,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 }
 
                 AppConfig.MSG_MEASURE_CONFIG_SUCCESS -> {
-                    val resultPair: Pair<String, Long> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                        intent.getSerializableExtra("content", Pair::class.java) as Pair<String, Long>
-                    } else {
-                        intent.getSerializableExtra("content") as Pair<String, Long>
-                    }
+                    val resultPair = intent.serializable<Pair<String, Long>>("content") ?: return
                     MmkvManager.encodeServerTestDelayMillis(resultPair.first, resultPair.second)
                     updateListAction.value = getPosition(resultPair.first)
                 }

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/dto/ConfigResult.kt
@@ -1,0 +1,9 @@
+package com.neko.v2ray.dto
+
+data class ConfigResult (
+    var status: Boolean,
+    var guid: String? = null,
+    var content: String = "",
+    var domainPort: String? = null,
+)
+

app/src/main/kotlin/com/neko/v2ray/service/V2RayTestService.kt
@@ -3,9 +3,12 @@ package com.neko.v2ray.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.google.gson.Gson
 import com.neko.v2ray.AppConfig.MSG_MEASURE_CONFIG
 import com.neko.v2ray.AppConfig.MSG_MEASURE_CONFIG_CANCEL
 import com.neko.v2ray.AppConfig.MSG_MEASURE_CONFIG_SUCCESS
+import com.neko.v2ray.dto.ConfigResult
+import com.neko.v2ray.extension.serializable
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.SpeedtestUtil
 import com.neko.v2ray.util.Utils
@@ -30,10 +33,11 @@ class V2RayTestService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when (intent?.getIntExtra("key", 0)) {
             MSG_MEASURE_CONFIG -> {
-                val contentPair = intent.getSerializableExtra("content") as Pair<String, String>
+                val content = intent.serializable<String>("content")
+                val config = Gson().fromJson(content, ConfigResult::class.java)
                 realTestScope.launch {
-                    val result = SpeedtestUtil.realPing(contentPair.second)
-                    MessageUtil.sendMsg2UI(this@V2RayTestService, MSG_MEASURE_CONFIG_SUCCESS, Pair(contentPair.first, result))
+                    val result = SpeedtestUtil.realPing(config.content)
+                    MessageUtil.sendMsg2UI(this@V2RayTestService, MSG_MEASURE_CONFIG_SUCCESS, Pair(config.guid, result))
                 }
             }
 

app/src/main/kotlin/com/neko/v2ray/util/V2rayConfigUtil.kt
@@ -14,6 +14,7 @@ import com.neko.v2ray.AppConfig.TAG_FRAGMENT
 import com.neko.v2ray.AppConfig.TAG_PROXY
 import com.neko.v2ray.AppConfig.WIREGUARD_LOCAL_ADDRESS_V4
 import com.neko.v2ray.AppConfig.WIREGUARD_LOCAL_ADDRESS_V6
+import com.neko.v2ray.dto.ConfigResult
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.RulesetItem
 import com.neko.v2ray.dto.ServerConfig
@@ -25,34 +26,32 @@ import com.neko.v2ray.util.MmkvManager.settingsStorage
 
 object V2rayConfigUtil {
 
-    data class Result(var status: Boolean, var content: String = "", var domainPort: String? = null)
-
-    fun getV2rayConfig(context: Context, guid: String): Result {
+    fun getV2rayConfig(context: Context, guid: String): ConfigResult {
         try {
-            val config = MmkvManager.decodeServerConfig(guid) ?: return Result(false)
+            val config = MmkvManager.decodeServerConfig(guid) ?: return ConfigResult(false)
             if (config.configType == EConfigType.CUSTOM) {
                 val raw = MmkvManager.decodeServerRaw(guid)
                 val customConfig = if (raw.isNullOrBlank()) {
-                    config.fullConfig?.toPrettyPrinting() ?: return Result(false)
+                    config.fullConfig?.toPrettyPrinting() ?: return ConfigResult(false)
                 } else {
                     raw
                 }
                 val domainPort = config.getProxyOutbound()?.getServerAddressAndPort()
-                return Result(true, customConfig, domainPort)
+                return ConfigResult(true, guid, customConfig, domainPort)
             }
 
             val result = getV2rayNonCustomConfig(context, config)
             //Log.d(ANG_PACKAGE, result.content)
-            Log.d(ANG_PACKAGE, result.domainPort ?: "")
+            result.guid = guid
             return result
         } catch (e: Exception) {
             e.printStackTrace()
-            return Result(false)
+            return ConfigResult(false)
         }
     }
 
-    private fun getV2rayNonCustomConfig(context: Context, config: ServerConfig): Result {
-        val result = Result(false)
+    private fun getV2rayNonCustomConfig(context: Context, config: ServerConfig): ConfigResult {
+        val result = ConfigResult(false)
 
         val outbound = config.getProxyOutbound() ?: return result
         val address = outbound.getServerAddress() ?: return result

app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
@@ -21,6 +21,7 @@ import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.ServerConfig
 import com.neko.v2ray.dto.ServersCache
 import com.neko.v2ray.dto.V2rayConfig
+import com.neko.v2ray.extension.serializable
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.util.AngConfigManager
 import com.neko.v2ray.util.AngConfigManager.updateConfigViaSub
@@ -217,7 +218,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                     MessageUtil.sendMsg2TestService(
                         getApplication(),
                         AppConfig.MSG_MEASURE_CONFIG,
-                        Pair(item.guid, config.content)
+                        Gson().toJson(config)
                     )
                 }
             }
@@ -395,11 +396,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 }
 
                 AppConfig.MSG_MEASURE_CONFIG_SUCCESS -> {
-                    val resultPair: Pair<String, Long> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                        intent.getSerializableExtra("content", Pair::class.java) as Pair<String, Long>
-                    } else {
-                        intent.getSerializableExtra("content") as Pair<String, Long>
-                    }
+                    val resultPair = intent.serializable<Pair<String, Long>>("content") ?: return
                     MmkvManager.encodeServerTestDelayMillis(resultPair.first, resultPair.second)
                     updateListAction.value = getPosition(resultPair.first)
                 }