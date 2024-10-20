3990a6138d4a25a6649ec1fb85471f0d89840024
AnGgIt86 <jarbull86@gmail.com>
2024-10-20 20:30:21 +0700

    Update plugins in libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.1" apply false
-    id("com.android.library") version "8.7.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.2.3" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.aboutlibraries) apply false
 }
