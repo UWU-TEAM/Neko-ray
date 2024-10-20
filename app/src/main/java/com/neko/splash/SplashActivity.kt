af13624237bb2cb5b26689c43857fd791adbf85c
AnGgIt86 <jarbull86@gmail.com>
2024-10-20 15:34:03 +0700

    Little change and fix

--- app/src/main/java/com/neko/splash/SplashActivity.kt
+++ app/src/main/java/com/neko/splash/SplashActivity.kt
@@ -1,11 +1,16 @@
 package com.neko.splash
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.database.Cursor
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+
+import androidx.annotation.ColorInt
+
 import com.neko.v2ray.R
 import com.neko.v2ray.ui.BaseActivity
 import com.neko.v2ray.ui.MainActivity
@@ -22,7 +27,8 @@ class SplashActivity : BaseActivity() {
         myDB = MyDatabaseHelper(this)
         
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        window.statusBarColor = Color.TRANSPARENT
 
         Handler(Looper.getMainLooper()).postDelayed({
             val cursor: Cursor? = myDB.bacaSemuaData()
