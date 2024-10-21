de76f78e61555eddb07e159eab813a31801537e9
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2024-10-21 12:43:48 +0600

    fix(): Fix subs id check

--- app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
@@ -43,7 +43,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     var subscriptionId: String = MmkvManager.settingsStorage.decodeString(AppConfig.CACHE_SUBSCRIPTION_ID, "").orEmpty()
 
     //var keywordFilter: String = MmkvManager.settingsStorage.decodeString(AppConfig.CACHE_KEYWORD_FILTER, "")?:""
-    var keywordFilter = ""
+    var  keywordFilter = ""
     val serversCache = mutableListOf<ServersCache>()
     val isRunning by lazy { MutableLiveData<Boolean>() }
     val updateListAction by lazy { MutableLiveData<Int>() }
@@ -154,7 +154,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun updateConfigViaSubAll(): Int {
-        if (subscriptionId.isNullOrEmpty()) {
+        if (subscriptionId.isEmpty()) {
             return AngConfigManager.updateConfigViaSubAll()
         } else {
             val subItem = MmkvManager.decodeSubscription(subscriptionId) ?: return 0
@@ -164,7 +164,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     fun exportAllServer(): Int {
         val serverListCopy =
-            if (subscriptionId.isNullOrEmpty() && keywordFilter.isNullOrEmpty()) {
+            if (subscriptionId.isEmpty() && keywordFilter.isNullOrEmpty()) {
                 serverList
             } else {
                 serversCache.map { it.guid }.toList()
@@ -282,7 +282,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun removeAllServer() {
-        if (subscriptionId.isNullOrEmpty() && keywordFilter.isNullOrEmpty()) {
+        if (subscriptionId.isEmpty() && keywordFilter.isNullOrEmpty()) {
             MmkvManager.removeAllServer()
         } else {
             val serversCopy = serversCache.toList()
@@ -293,7 +293,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun removeInvalidServer() {
-        if (subscriptionId.isNullOrEmpty() && keywordFilter.isNullOrEmpty()) {
+        if (subscriptionId.isEmpty() && keywordFilter.isNullOrEmpty()) {
             MmkvManager.removeInvalidServer("")
         } else {
             val serversCopy = serversCache.toList()
