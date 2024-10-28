a74e64f49d3812caef6811c135c171f5586f85ef
2dust <31833384+2dust@users.noreply.github.com>
2024-10-28 12:05:17 +0800

    docs(): Test connection after updating subscription

--- app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
+++ app/src/main/kotlin/com/neko/v2ray/viewmodel/MainViewModel.kt
@@ -23,7 +23,6 @@ import com.neko.v2ray.dto.V2rayConfig
 import com.neko.v2ray.extension.serializable
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.util.AngConfigManager
-import com.neko.v2ray.util.AngConfigManager.updateConfigViaSub
 import com.neko.v2ray.util.JsonUtil
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.MmkvManager
@@ -158,7 +157,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             return AngConfigManager.updateConfigViaSubAll()
         } else {
             val subItem = MmkvManager.decodeSubscription(subscriptionId) ?: return 0
-            return updateConfigViaSub(Pair(subscriptionId, subItem))
+            return AngConfigManager.updateConfigViaSub(Pair(subscriptionId, subItem))
         }
     }
 
@@ -182,10 +181,10 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         tcpingTestScope.coroutineContext[Job]?.cancelChildren()
         SpeedtestUtil.closeAllTcpSockets()
         MmkvManager.clearAllTestDelayResults(serversCache.map { it.guid }.toList())
-        updateListAction.value = -1 // update all
+        //updateListAction.value = -1 // update all
 
-        getApplication<AngApplication>().toast(R.string.connection_test_testing)
-        for (item in serversCache) {
+        val serversCopy = serversCache.toList() // Create a copy of the list
+        for (item in serversCopy) {
             item.profile.let { outbound ->
                 val serverAddress = outbound.server
                 val serverPort = outbound.serverPort
@@ -208,8 +207,6 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         updateListAction.value = -1 // update all
 
         val serversCopy = serversCache.toList() // Create a copy of the list
-
-        getApplication<AngApplication>().toast(R.string.connection_test_testing)
         viewModelScope.launch(Dispatchers.Default) { // without Dispatchers.Default viewModelScope will launch in main thread
             for (item in serversCopy) {
                 MessageUtil.sendMsg2TestService(getApplication(), AppConfig.MSG_MEASURE_CONFIG, item.guid)
