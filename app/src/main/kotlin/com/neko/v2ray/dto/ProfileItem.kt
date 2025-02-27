2dust/v2rayNG

Refactor server configuration storage

V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/ProfileItem.kt
@@ -1,0 +1,72 @@
+package com.v2ray.ang.dto
+
+import com.v2ray.ang.AppConfig.TAG_BLOCKED
+import com.v2ray.ang.AppConfig.TAG_DIRECT
+import com.v2ray.ang.AppConfig.TAG_PROXY
+import com.v2ray.ang.util.JsonUtil
+import com.v2ray.ang.util.Utils
+
+data class ProfileItem(
+    val configVersion: Int = 4,
+    val configType: EConfigType,
+    var subscriptionId: String = "",
+    var addedTime: Long = System.currentTimeMillis(),
+
+    var remarks: String = "",
+    var server: String? = null,
+    var serverPort: String? = null,
+
+    var password: String? = null,
+    var method: String? = null,
+    var flow: String? = null,
+    var username: String? = null,
+
+    var network: String? = null,
+    var headerType: String? = null,
+    var host: String? = null,
+    var path: String? = null,
+    var seed: String? = null,
+    var quicSecurity: String? = null,
+    var quicKey: String? = null,
+    var mode: String? = null,
+    var serviceName: String? = null,
+    var authority: String? = null,
+
+    var security: String? = null,
+    var sni: String? = null,
+    var alpn: String? = null,
+    var fingerPrint: String? = null,
+    var insecure: Boolean? = null,
+
+    var publicKey: String? = null,
+    var shortId: String? = null,
+    var spiderX: String? = null,
+
+    var secretKey: String? = null,
+    var localAddress: String? = null,
+    var reserved: String? = null,
+    var mtu: Int? = null,
+
+    var obfsPassword: String? = null,
+
+    ) {
+    companion object {
+        fun create(configType: EConfigType): ProfileItem {
+            return ProfileItem(configType = configType)
+        }
+    }
+
+    fun getAllOutboundTags(): MutableList<String> {
+        return mutableListOf(TAG_PROXY, TAG_DIRECT, TAG_BLOCKED)
+    }
+
+    fun getServerAddressAndPort(): String {
+        return Utils.getIpv6Address(server) + ":" + serverPort
+    }
+
+    fun getKeyProperty(): String {
+        subscriptionId = ""
+        addedTime = 0L
+        return JsonUtil.toJson(this)
+    }
+}

V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayServiceManager.kt
@@ -18,7 +18,7 @@ import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.AppConfig.TAG_DIRECT
 import com.v2ray.ang.AppConfig.VPN
 import com.v2ray.ang.R
-import com.v2ray.ang.dto.ServerConfig
+import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.extension.toSpeedString
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.ui.MainActivity
@@ -55,7 +55,7 @@ object V2RayServiceManager {
             Seq.setContext(value?.get()?.getService()?.applicationContext)
             Libv2ray.initV2Env(Utils.userAssetPath(value?.get()?.getService()), Utils.getDeviceIdForXUDPBaseKey())
         }
-    var currentConfig: ServerConfig? = null
+    var currentConfig: ProfileItem? = null
 
     private var lastQueryTime = 0L
     private var mBuilder: NotificationCompat.Builder? = null
@@ -65,8 +65,10 @@ object V2RayServiceManager {
     fun startV2Ray(context: Context) {
         if (v2rayPoint.isRunning) return
         val guid = MmkvManager.getSelectServer() ?: return
-        val result = V2rayConfigUtil.getV2rayConfig(context, guid)
-        if (!result.status) return
+        val config = MmkvManager.decodeServerConfig(guid) ?: return
+        if (!Utils.isValidUrl(config.server) && !Utils.isValidUrl(config.server)) return
+//        val result = V2rayConfigUtil.getV2rayConfig(context, guid)
+//        if (!result.status) return
 
         if (settingsStorage?.decodeBool(AppConfig.PREF_PROXY_SHARING) == true) {
             context.toast(R.string.toast_warning_pref_proxysharing_short)

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/AngConfigManager.kt
@@ -4,16 +4,11 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.text.TextUtils
 import android.util.Log
-
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonPrimitive
-import com.google.gson.JsonSerializationContext
-import com.google.gson.JsonSerializer
-import com.google.gson.reflect.TypeToken
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.AppConfig.HY2
 import com.v2ray.ang.R
 import com.v2ray.ang.dto.*
+import com.v2ray.ang.util.fmt.CustomFmt
 import com.v2ray.ang.util.fmt.Hysteria2Fmt
 import com.v2ray.ang.util.fmt.ShadowsocksFmt
 import com.v2ray.ang.util.fmt.SocksFmt
@@ -21,9 +16,7 @@ import com.v2ray.ang.util.fmt.TrojanFmt
 import com.v2ray.ang.util.fmt.VlessFmt
 import com.v2ray.ang.util.fmt.VmessFmt
 import com.v2ray.ang.util.fmt.WireguardFmt
-import java.lang.reflect.Type
 import java.net.URI
-import java.util.*
 
 object AngConfigManager {
     /**
@@ -33,7 +26,7 @@ object AngConfigManager {
         str: String?,
         subid: String,
         subItem: SubscriptionItem?,
-        removedSelectedServer: ServerConfig?
+        removedSelectedServer: ProfileItem?
     ): Int {
         try {
             if (str == null || TextUtils.isEmpty(str)) {
@@ -71,12 +64,7 @@ object AngConfigManager {
             config.subscriptionId = subid
             val guid = MmkvManager.encodeServerConfig("", config)
             if (removedSelectedServer != null &&
-                config.getProxyOutbound()
-                    ?.getServerAddress() == removedSelectedServer.getProxyOutbound()
-                    ?.getServerAddress() &&
-                config.getProxyOutbound()
-                    ?.getServerPort() == removedSelectedServer.getProxyOutbound()
-                    ?.getServerPort()
+                config.server == removedSelectedServer.server && config.serverPort == removedSelectedServer.serverPort
             ) {
                 MmkvManager.setSelectServer(guid)
             }
@@ -284,12 +272,7 @@ object AngConfigManager {
                 if (serverList.isNotEmpty()) {
                     var count = 0
                     for (srv in serverList.reversed()) {
-                        val config = ServerConfig.create(EConfigType.CUSTOM)
-                        config.fullConfig =
-                            JsonUtil.fromJson(JsonUtil.toJson(srv), V2rayConfig::class.java)
-                        config.remarks = config.fullConfig?.remarks
-                            ?: ("%04d-".format(count + 1) + System.currentTimeMillis()
-                                .toString())
+                        val config = CustomFmt.parse(server) ?: continue
                         config.subscriptionId = subid
                         val key = MmkvManager.encodeServerConfig("", config)
                         MmkvManager.encodeServerRaw(key, JsonUtil.toJsonPretty(srv))
@@ -303,10 +286,8 @@ object AngConfigManager {
 
             try {
                 // For compatibility
-                val config = ServerConfig.create(EConfigType.CUSTOM)
+                val config = CustomFmt.parse(server) ?: return 0
                 config.subscriptionId = subid
-                config.fullConfig = JsonUtil.fromJson(server, V2rayConfig::class.java)
-                config.remarks = config.fullConfig?.remarks ?: System.currentTimeMillis().toString()
                 val key = MmkvManager.encodeServerConfig("", config)
                 MmkvManager.encodeServerRaw(key, server)
                 return 1
@@ -316,9 +297,7 @@ object AngConfigManager {
             return 0
         } else if (server.startsWith("[Interface]") && server.contains("[Peer]")) {
             try {
-                val config = WireguardFmt.parseWireguardConfFile(server)
-                    ?: return R.string.toast_incorrect_protocol
-                config.fullConfig?.remarks ?: System.currentTimeMillis().toString()
+                val config = WireguardFmt.parseWireguardConfFile(server) ?: return R.string.toast_incorrect_protocol
                 val key = MmkvManager.encodeServerConfig("", config)
                 MmkvManager.encodeServerRaw(key, server)
                 return 1

V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/SettingsManager.kt
@@ -2,20 +2,19 @@ package com.v2ray.ang.util
 
 import android.content.Context
 import android.text.TextUtils
-
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.AppConfig.GEOIP_PRIVATE
 import com.v2ray.ang.AppConfig.GEOSITE_PRIVATE
 import com.v2ray.ang.AppConfig.TAG_DIRECT
+import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.RoutingType
 import com.v2ray.ang.dto.RulesetItem
-import com.v2ray.ang.dto.ServerConfig
-import com.v2ray.ang.util.MmkvManager.decodeProfileConfig
 import com.v2ray.ang.util.MmkvManager.decodeServerConfig
 import com.v2ray.ang.util.MmkvManager.decodeServerList
 import com.v2ray.ang.util.MmkvManager.settingsStorage
 import com.v2ray.ang.util.Utils.parseInt
 import java.util.Collections
+import kotlin.Int
 
 object SettingsManager {
 
@@ -131,15 +130,15 @@ object SettingsManager {
         MmkvManager.encodeSubsList(subsList)
     }
 
-    fun getServerViaRemarks(remarks: String?): ServerConfig? {
+    fun getServerViaRemarks(remarks: String?): ProfileItem? {
         if (remarks == null) {
             return null
         }
         val serverList = decodeServerList()
         for (guid in serverList) {
-            val profile = decodeProfileConfig(guid)
+            val profile = decodeServerConfig(guid)
             if (profile != null && profile.remarks == remarks) {
-                return decodeServerConfig(guid)
+                return profile
             }
         }
         return null

UWU-TEAM/Neko-ray

app/src/main/kotlin/com/neko/v2ray/dto/ProfileItem.kt
@@ -1,0 +1,72 @@
+package com.neko.v2ray.dto
+
+import com.neko.v2ray.AppConfig.TAG_BLOCKED
+import com.neko.v2ray.AppConfig.TAG_DIRECT
+import com.neko.v2ray.AppConfig.TAG_PROXY
+import com.neko.v2ray.util.JsonUtil
+import com.neko.v2ray.util.Utils
+
+data class ProfileItem(
+    val configVersion: Int = 4,
+    val configType: EConfigType,
+    var subscriptionId: String = "",
+    var addedTime: Long = System.currentTimeMillis(),
+
+    var remarks: String = "",
+    var server: String? = null,
+    var serverPort: String? = null,
+
+    var password: String? = null,
+    var method: String? = null,
+    var flow: String? = null,
+    var username: String? = null,
+
+    var network: String? = null,
+    var headerType: String? = null,
+    var host: String? = null,
+    var path: String? = null,
+    var seed: String? = null,
+    var quicSecurity: String? = null,
+    var quicKey: String? = null,
+    var mode: String? = null,
+    var serviceName: String? = null,
+    var authority: String? = null,
+
+    var security: String? = null,
+    var sni: String? = null,
+    var alpn: String? = null,
+    var fingerPrint: String? = null,
+    var insecure: Boolean? = null,
+
+    var publicKey: String? = null,
+    var shortId: String? = null,
+    var spiderX: String? = null,
+
+    var secretKey: String? = null,
+    var localAddress: String? = null,
+    var reserved: String? = null,
+    var mtu: Int? = null,
+
+    var obfsPassword: String? = null,
+
+    ) {
+    companion object {
+        fun create(configType: EConfigType): ProfileItem {
+            return ProfileItem(configType = configType)
+        }
+    }
+
+    fun getAllOutboundTags(): MutableList<String> {
+        return mutableListOf(TAG_PROXY, TAG_DIRECT, TAG_BLOCKED)
+    }
+
+    fun getServerAddressAndPort(): String {
+        return Utils.getIpv6Address(server) + ":" + serverPort
+    }
+
+    fun getKeyProperty(): String {
+        subscriptionId = ""
+        addedTime = 0L
+        return JsonUtil.toJson(this)
+    }
+}

app/src/main/kotlin/com/neko/v2ray/service/V2RayServiceManager.kt
@@ -18,7 +18,7 @@ import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.AppConfig.TAG_DIRECT
 import com.neko.v2ray.AppConfig.VPN
 import com.neko.v2ray.R
-import com.neko.v2ray.dto.ServerConfig
+import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.extension.toSpeedString
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.ui.MainActivity
@@ -55,7 +55,7 @@ object V2RayServiceManager {
             Seq.setContext(value?.get()?.getService()?.applicationContext)
             Libv2ray.initV2Env(Utils.userAssetPath(value?.get()?.getService()), Utils.getDeviceIdForXUDPBaseKey())
         }
-    var currentConfig: ServerConfig? = null
+    var currentConfig: ProfileItem? = null
 
     private var lastQueryTime = 0L
     private var mBuilder: NotificationCompat.Builder? = null
@@ -65,8 +65,10 @@ object V2RayServiceManager {
     fun startV2Ray(context: Context) {
         if (v2rayPoint.isRunning) return
         val guid = MmkvManager.getSelectServer() ?: return
-        val result = V2rayConfigUtil.getV2rayConfig(context, guid)
-        if (!result.status) return
+        val config = MmkvManager.decodeServerConfig(guid) ?: return
+        if (!Utils.isValidUrl(config.server) && !Utils.isValidUrl(config.server)) return
+//        val result = V2rayConfigUtil.getV2rayConfig(context, guid)
+//        if (!result.status) return
 
         if (settingsStorage?.decodeBool(AppConfig.PREF_PROXY_SHARING) == true) {
             context.toast(R.string.toast_warning_pref_proxysharing_short)

app/src/main/kotlin/com/neko/v2ray/util/AngConfigManager.kt
@@ -5,15 +5,11 @@ import android.graphics.Bitmap
 import android.text.TextUtils
 import android.util.Log
 
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonPrimitive
-import com.google.gson.JsonSerializationContext
-import com.google.gson.JsonSerializer
-import com.google.gson.reflect.TypeToken
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.AppConfig.HY2
 import com.neko.v2ray.R
 import com.neko.v2ray.dto.*
+import com.neko.v2ray.util.fmt.CustomFmt
 import com.neko.v2ray.util.fmt.Hysteria2Fmt
 import com.neko.v2ray.util.fmt.ShadowsocksFmt
 import com.neko.v2ray.util.fmt.SocksFmt
@@ -21,9 +17,7 @@ import com.neko.v2ray.util.fmt.TrojanFmt
 import com.neko.v2ray.util.fmt.VlessFmt
 import com.neko.v2ray.util.fmt.VmessFmt
 import com.neko.v2ray.util.fmt.WireguardFmt
-import java.lang.reflect.Type
 import java.net.URI
-import java.util.*
 
 object AngConfigManager {
     /**
@@ -33,7 +27,7 @@ object AngConfigManager {
         str: String?,
         subid: String,
         subItem: SubscriptionItem?,
-        removedSelectedServer: ServerConfig?
+        removedSelectedServer: ProfileItem?
     ): Int {
         try {
             if (str == null || TextUtils.isEmpty(str)) {
@@ -70,12 +64,7 @@ object AngConfigManager {
             config.subscriptionId = subid
             val guid = MmkvManager.encodeServerConfig("", config)
             if (removedSelectedServer != null &&
-                config.getProxyOutbound()
-                    ?.getServerAddress() == removedSelectedServer.getProxyOutbound()
-                    ?.getServerAddress() &&
-                config.getProxyOutbound()
-                    ?.getServerPort() == removedSelectedServer.getProxyOutbound()
-                    ?.getServerPort()
+                config.server == removedSelectedServer.server && config.serverPort == removedSelectedServer.serverPort
             ) {
                 MmkvManager.setSelectServer(guid)
             }
@@ -283,12 +272,7 @@ object AngConfigManager {
                 if (serverList.isNotEmpty()) {
                     var count = 0
                     for (srv in serverList.reversed()) {
-                        val config = ServerConfig.create(EConfigType.CUSTOM)
-                        config.fullConfig =
-                            JsonUtil.fromJson(JsonUtil.toJson(srv), V2rayConfig::class.java)
-                        config.remarks = config.fullConfig?.remarks
-                            ?: ("%04d-".format(count + 1) + System.currentTimeMillis()
-                                .toString())
+                        val config = CustomFmt.parse(server) ?: continue
                         config.subscriptionId = subid
                         val key = MmkvManager.encodeServerConfig("", config)
                         MmkvManager.encodeServerRaw(key, JsonUtil.toJsonPretty(srv))
@@ -302,10 +286,8 @@ object AngConfigManager {
 
             try {
                 // For compatibility
-                val config = ServerConfig.create(EConfigType.CUSTOM)
+                val config = CustomFmt.parse(server) ?: return 0
                 config.subscriptionId = subid
-                config.fullConfig = JsonUtil.fromJson(server, V2rayConfig::class.java)
-                config.remarks = config.fullConfig?.remarks ?: System.currentTimeMillis().toString()
                 val key = MmkvManager.encodeServerConfig("", config)
                 MmkvManager.encodeServerRaw(key, server)
                 return 1
@@ -315,9 +297,7 @@ object AngConfigManager {
             return 0
         } else if (server.startsWith("[Interface]") && server.contains("[Peer]")) {
             try {
-                val config = WireguardFmt.parseWireguardConfFile(server)
-                    ?: return R.string.toast_incorrect_protocol
-                config.fullConfig?.remarks ?: System.currentTimeMillis().toString()
+                val config = WireguardFmt.parseWireguardConfFile(server) ?: return R.string.toast_incorrect_protocol
                 val key = MmkvManager.encodeServerConfig("", config)
                 MmkvManager.encodeServerRaw(key, server)
                 return 1

app/src/main/kotlin/com/neko/v2ray/util/SettingsManager.kt
@@ -2,20 +2,19 @@ package com.neko.v2ray.util
 
 import android.content.Context
 import android.text.TextUtils
-
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.AppConfig.GEOIP_PRIVATE
 import com.neko.v2ray.AppConfig.GEOSITE_PRIVATE
 import com.neko.v2ray.AppConfig.TAG_DIRECT
+import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.RoutingType
 import com.neko.v2ray.dto.RulesetItem
-import com.neko.v2ray.dto.ServerConfig
-import com.neko.v2ray.util.MmkvManager.decodeProfileConfig
 import com.neko.v2ray.util.MmkvManager.decodeServerConfig
 import com.neko.v2ray.util.MmkvManager.decodeServerList
 import com.neko.v2ray.util.MmkvManager.settingsStorage
 import com.neko.v2ray.util.Utils.parseInt
 import java.util.Collections
+import kotlin.Int
 
 object SettingsManager {
 
@@ -131,15 +130,15 @@ object SettingsManager {
         MmkvManager.encodeSubsList(subsList)
     }
 
-    fun getServerViaRemarks(remarks: String?): ServerConfig? {
+    fun getServerViaRemarks(remarks: String?): ProfileItem? {
         if (remarks == null) {
             return null
         }
         val serverList = decodeServerList()
         for (guid in serverList) {
-            val profile = decodeProfileConfig(guid)
+            val profile = decodeServerConfig(guid)
             if (profile != null && profile.remarks == remarks) {
-                return decodeServerConfig(guid)
+                return profile
             }
         }
         return null