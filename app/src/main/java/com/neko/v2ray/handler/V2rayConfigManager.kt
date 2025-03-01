2dust/v2rayNG

Disable mux when using xhttp

V2rayNG/app/src/main/java/com/v2ray/ang/handler/V2rayConfigManager.kt
@@ -34,6 +34,7 @@ import com.v2ray.ang.AppConfig.WIREGUARD_LOCAL_ADDRESS_V4
 import com.v2ray.ang.AppConfig.WIREGUARD_LOCAL_ADDRESS_V6
 import com.v2ray.ang.dto.ConfigResult
 import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.NetworkType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.RulesetItem
 import com.v2ray.ang.dto.V2rayConfig
@@ -425,6 +426,8 @@ object V2rayConfigManager {
             } else if (protocol.equals(EConfigType.VLESS.name, true)
                 && outbound.settings?.vnext?.first()?.users?.first()?.flow?.isNotEmpty() == true
             ) {
+                muxEnabled = false
+            } else if (outbound.streamSettings?.network == NetworkType.XHTTP.type) {
                 muxEnabled = false
             }
             if (muxEnabled == true) {

UWU-TEAM/Neko-ray

app/src/main/java/com/neko/v2ray/handler/V2rayConfigManager.kt
@@ -34,6 +34,7 @@ import com.neko.v2ray.AppConfig.WIREGUARD_LOCAL_ADDRESS_V4
 import com.neko.v2ray.AppConfig.WIREGUARD_LOCAL_ADDRESS_V6
 import com.neko.v2ray.dto.ConfigResult
 import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.NetworkType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.RulesetItem
 import com.neko.v2ray.dto.V2rayConfig
@@ -425,6 +426,8 @@ object V2rayConfigManager {
             } else if (protocol.equals(EConfigType.VLESS.name, true)
                 && outbound.settings?.vnext?.first()?.users?.first()?.flow?.isNotEmpty() == true
             ) {
+                muxEnabled = false
+            } else if (outbound.streamSettings?.network == NetworkType.XHTTP.type) {
                 muxEnabled = false
             }
             if (muxEnabled == true) {