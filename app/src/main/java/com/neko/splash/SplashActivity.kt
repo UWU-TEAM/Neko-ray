0ca82e4b06040c16b7610a73b378972e95fb7967
UntuKemeng <jarbull87@gmail.com>
2025-03-01 21:11:19 +0700

    Fix splashscreen fullscreen with cutout

--- app/src/main/java/com/neko/splash/SplashActivity.kt
+++ app/src/main/java/com/neko/splash/SplashActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.content.res.ColorStateList
 import android.database.Cursor
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.WindowManager
 
 import androidx.annotation.ColorInt
 
@@ -25,10 +27,14 @@ class SplashActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.uwu_activity_splash)
         myDB = MyDatabaseHelper(this)
-        
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-        window.statusBarColor = Color.TRANSPARENT
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            window.statusBarColor = Color.TRANSPARENT
+            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        } else { window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            window.statusBarColor = Color.TRANSPARENT
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             val cursor: Cursor? = myDB.bacaSemuaData()
