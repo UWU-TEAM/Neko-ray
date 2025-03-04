2dust/v2rayNG

Custom configuration can use any outbound
https://github.com/2dust/v2rayNG/issues/4243

V2rayNG/app/src/main/java/com/v2ray/ang/dto/ProfileItem.kt
@@ -1,5 +1,7 @@
 package com.v2ray.ang.dto
 
+import com.v2ray.ang.AppConfig.LOOPBACK
+import com.v2ray.ang.AppConfig.PORT_SOCKS
 import com.v2ray.ang.AppConfig.TAG_BLOCKED
 import com.v2ray.ang.AppConfig.TAG_DIRECT
 import com.v2ray.ang.AppConfig.TAG_PROXY
@@ -66,6 +68,9 @@ data class ProfileItem(
     }
 
     fun getServerAddressAndPort(): String {
+        if (server.isNullOrEmpty() && configType == EConfigType.CUSTOM) {
+            return "$LOOPBACK:$PORT_SOCKS"
+        }
         return Utils.getIpv6Address(server) + ":" + serverPort
     }
 

V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayServiceManager.kt
@@ -20,6 +20,7 @@ import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.AppConfig.TAG_DIRECT
 import com.v2ray.ang.AppConfig.VPN
 import com.v2ray.ang.R
+import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.extension.toSpeedString
 import com.v2ray.ang.extension.toast
@@ -68,7 +69,10 @@ object V2RayServiceManager {
         if (v2rayPoint.isRunning) return
         val guid = MmkvManager.getSelectServer() ?: return
         val config = MmkvManager.decodeServerConfig(guid) ?: return
-        if (!Utils.isValidUrl(config.server) && !Utils.isIpAddress(config.server)) return
+        if (config.configType != EConfigType.CUSTOM
+            && !Utils.isValidUrl(config.server)
+            && !Utils.isIpAddress(config.server)
+        ) return
 //        val result = V2rayConfigUtil.getV2rayConfig(context, guid)
 //        if (!result.status) return
 

UWU-TEAM/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/dto/ProfileItem.kt
@@ -1,5 +1,7 @@
 package com.neko.v2ray.dto
 
+import com.neko.v2ray.AppConfig.LOOPBACK
+import com.neko.v2ray.AppConfig.PORT_SOCKS
 import com.neko.v2ray.AppConfig.TAG_BLOCKED
 import com.neko.v2ray.AppConfig.TAG_DIRECT
 import com.neko.v2ray.AppConfig.TAG_PROXY
@@ -66,6 +68,9 @@ data class ProfileItem(
     }
 
     fun getServerAddressAndPort(): String {
+        if (server.isNullOrEmpty() && configType == EConfigType.CUSTOM) {
+            return "$LOOPBACK:$PORT_SOCKS"
+        }
         return Utils.getIpv6Address(server) + ":" + serverPort
     }
 

V2rayNG/app/src/main/java/com/neko/v2ray/service/V2RayServiceManager.kt
@@ -20,6 +20,7 @@ import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.AppConfig.TAG_DIRECT
 import com.neko.v2ray.AppConfig.VPN
 import com.neko.v2ray.R
+import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.extension.toSpeedString
 import com.neko.v2ray.extension.toast
@@ -68,7 +69,10 @@ object V2RayServiceManager {
         if (v2rayPoint.isRunning) return
         val guid = MmkvManager.getSelectServer() ?: return
         val config = MmkvManager.decodeServerConfig(guid) ?: return
-        if (!Utils.isValidUrl(config.server) && !Utils.isIpAddress(config.server)) return
+        if (config.configType != EConfigType.CUSTOM
+            && !Utils.isValidUrl(config.server)
+            && !Utils.isIpAddress(config.server)
+        ) return
 //        val result = V2rayConfigUtil.getV2rayConfig(context, guid)
 //        if (!result.status) return
 