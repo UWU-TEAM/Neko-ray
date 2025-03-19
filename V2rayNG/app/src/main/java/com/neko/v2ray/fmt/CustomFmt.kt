abf0c2187ace48c37de77440193d1a38cac83ede
2dust <31833384+2dust@users.noreply.github.com>
2025-03-19 12:08:19 +0800

    Using AI to improve function documentation

--- V2rayNG/app/src/main/java/com/neko/v2ray/fmt/CustomFmt.kt
+++ V2rayNG/app/src/main/java/com/neko/v2ray/fmt/CustomFmt.kt
@@ -6,6 +6,12 @@ import com.neko.v2ray.dto.V2rayConfig
 import com.neko.v2ray.util.JsonUtil
 
 object CustomFmt : FmtBase() {
+    /**
+     * Parses a JSON string into a ProfileItem object.
+     *
+     * @param str the JSON string to parse
+     * @return the parsed ProfileItem object, or null if parsing fails
+     */
     fun parse(str: String): ProfileItem? {
         val config = ProfileItem.create(EConfigType.CUSTOM)
 
