2dust/v2rayNG

Migrate SpeedtestUtil to SpeedtestManager

V2rayNG/app/src/main/java/com/v2ray/ang/handler/SpeedtestManager.kt
@@ -1,4 +1,4 @@
-package com.v2ray.ang.util
+package com.v2ray.ang.handler
 
 import android.content.Context
 import android.os.SystemClock
@@ -7,7 +7,7 @@ import android.util.Log
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.extension.responseLength
-import com.v2ray.ang.handler.SettingsManager
+import com.v2ray.ang.util.HttpUtil
 import kotlinx.coroutines.isActive
 import libv2ray.Libv2ray
 import java.io.IOException
@@ -16,7 +16,7 @@ import java.net.Socket
 import java.net.UnknownHostException
 import kotlin.coroutines.coroutineContext
 
-object SpeedtestUtil {
+object SpeedtestManager {
 
     private val tcpTestingSockets = ArrayList<Socket?>()
 

V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayTestService.kt
@@ -12,7 +12,7 @@ import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.handler.V2rayConfigManager
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.PluginUtil
-import com.v2ray.ang.util.SpeedtestUtil
+import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.util.Utils
 import go.Seq
 import kotlinx.coroutines.CoroutineScope
@@ -65,7 +65,7 @@ class V2RayTestService : Service() {
             if (!config.status) {
                 return retFailure
             }
-            return SpeedtestUtil.realPing(config.content)
+            return SpeedtestManager.realPing(config.content)
         }
     }
 }

V2rayNG/app/src/main/java/com/v2ray/ang/ui/AboutActivity.kt
@@ -14,7 +14,7 @@ import com.v2ray.ang.AppConfig
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityAboutBinding
-import com.v2ray.ang.util.SpeedtestUtil
+import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.util.ZipUtil
 import java.io.File
@@ -121,7 +121,7 @@ class AboutActivity : BaseActivity() {
             Utils.openUri(this, AppConfig.v2rayNGPrivacyPolicy)
         }
 
-        "v${BuildConfig.VERSION_NAME} (${SpeedtestUtil.getLibVersion()})".also {
+        "v${BuildConfig.VERSION_NAME} (${SpeedtestManager.getLibVersion()})".also {
             binding.tvVersion.text = it
         }
     }

V2rayNG/app/src/main/java/com/v2ray/ang/util/PluginUtil.kt
@@ -7,6 +7,7 @@ import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.fmt.Hysteria2Fmt
+import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.service.ProcessService
 import java.io.File
 
@@ -49,7 +50,7 @@ object PluginUtil {
             val proc = ProcessService()
             proc.runProcess(context, cmd)
             Thread.sleep(1000L)
-            val delay = SpeedtestUtil.testConnection(context, socksPort)
+            val delay = SpeedtestManager.testConnection(context, socksPort)
             proc.stopProcess()
 
             return delay.first

UWU-TEAM/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/handler/SpeedtestManager.kt
@@ -1,4 +1,4 @@
-package com.neko.v2ray.util
+package com.neko.v2ray.handler
 
 import android.content.Context
 import android.os.SystemClock
@@ -7,7 +7,7 @@ import android.util.Log
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.extension.responseLength
-import com.neko.v2ray.handler.SettingsManager
+import com.neko.v2ray.util.HttpUtil
 import kotlinx.coroutines.isActive
 import libv2ray.Libv2ray
 import java.io.IOException
@@ -16,7 +16,7 @@ import java.net.Socket
 import java.net.UnknownHostException
 import kotlin.coroutines.coroutineContext
 
-object SpeedtestUtil {
+object SpeedtestManager {
 
     private val tcpTestingSockets = ArrayList<Socket?>()
 

V2rayNG/app/src/main/java/com/neko/v2ray/service/V2RayTestService.kt
@@ -12,7 +12,7 @@ import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.handler.V2rayConfigManager
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.PluginUtil
-import com.neko.v2ray.util.SpeedtestUtil
+import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.util.Utils
 import go.Seq
 import kotlinx.coroutines.CoroutineScope
@@ -65,7 +65,7 @@ class V2RayTestService : Service() {
             if (!config.status) {
                 return retFailure
             }
-            return SpeedtestUtil.realPing(config.content)
+            return SpeedtestManager.realPing(config.content)
         }
     }
 }

V2rayNG/app/src/main/java/com/neko/v2ray/ui/AboutActivity.kt
@@ -14,7 +14,7 @@ import com.neko.v2ray.AppConfig
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityAboutBinding
-import com.neko.v2ray.util.SpeedtestUtil
+import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.util.ZipUtil
 import java.io.File
@@ -111,7 +111,7 @@ class AboutActivity : BaseActivity() {
             Utils.openUri(this, AppConfig.v2rayNGPrivacyPolicy)
         }
 
-        "v${BuildConfig.VERSION_NAME} (${SpeedtestUtil.getLibVersion()})".also {
+        "v${BuildConfig.VERSION_NAME} (${SpeedtestManager.getLibVersion()})".also {
             binding.tvVersion.text = it
         }
     }

V2rayNG/app/src/main/java/com/neko/v2ray/util/PluginUtil.kt
@@ -7,6 +7,7 @@ import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.fmt.Hysteria2Fmt
+import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.service.ProcessService
 import java.io.File
 
@@ -49,7 +50,7 @@ object PluginUtil {
             val proc = ProcessService()
             proc.runProcess(context, cmd)
             Thread.sleep(1000L)
-            val delay = SpeedtestUtil.testConnection(context, socksPort)
+            val delay = SpeedtestManager.testConnection(context, socksPort)
             proc.stopProcess()
 
             return delay.first