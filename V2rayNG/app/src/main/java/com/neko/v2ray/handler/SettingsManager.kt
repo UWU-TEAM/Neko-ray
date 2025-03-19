2dust/v2rayNG

Optimize and improve SettingsManager

V2rayNG/app/src/main/java/com/v2ray/ang/handler/SettingsManager.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.content.res.AssetManager
 import android.text.TextUtils
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.AppConfig.GEOIP_PRIVATE
 import com.v2ray.ang.AppConfig.GEOSITE_PRIVATE
 import com.v2ray.ang.AppConfig.TAG_DIRECT
 import com.v2ray.ang.dto.EConfigType
+import com.v2ray.ang.dto.Language
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.RoutingType
 import com.v2ray.ang.dto.RulesetItem
@@ -18,10 +20,10 @@ import com.v2ray.ang.handler.MmkvManager.decodeServerConfig
 import com.v2ray.ang.handler.MmkvManager.decodeServerList
 import com.v2ray.ang.util.JsonUtil
 import com.v2ray.ang.util.Utils
-import com.v2ray.ang.util.Utils.parseInt
 import java.io.File
 import java.io.FileOutputStream
 import java.util.Collections
+import java.util.Locale
 
 object SettingsManager {
 
@@ -173,7 +175,7 @@ object SettingsManager {
     }
 
     fun getSocksPort(): Int {
-        return parseInt(MmkvManager.decodeSettingsString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
+        return Utils.parseInt(MmkvManager.decodeSettingsString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
     }
 
     fun getHttpPort(): Int {
@@ -204,5 +206,76 @@ object SettingsManager {
             Log.e(ANG_PACKAGE, "asset copy failed", e)
         }
 
+    }
+
+    /**
+     * get domestic dns servers from preference
+     */
+    fun getDomesticDnsServers(): List<String> {
+        val domesticDns =
+            MmkvManager.decodeSettingsString(AppConfig.PREF_DOMESTIC_DNS) ?: AppConfig.DNS_DIRECT
+        val ret = domesticDns.split(",").filter { Utils.isPureIpAddress(it) || Utils.isCoreDNSAddress(it) }
+        if (ret.isEmpty()) {
+            return listOf(AppConfig.DNS_DIRECT)
+        }
+        return ret
+    }
+
+    /**
+     * get remote dns servers from preference
+     */
+    fun getRemoteDnsServers(): List<String> {
+        val remoteDns =
+            MmkvManager.decodeSettingsString(AppConfig.PREF_REMOTE_DNS) ?: AppConfig.DNS_PROXY
+        val ret = remoteDns.split(",").filter { Utils.isPureIpAddress(it) || Utils.isCoreDNSAddress(it) }
+        if (ret.isEmpty()) {
+            return listOf(AppConfig.DNS_PROXY)
+        }
+        return ret
+    }
+
+    /**
+     * get vpn dns servers from preference
+     */
+    fun getVpnDnsServers(): List<String> {
+        val vpnDns = MmkvManager.decodeSettingsString(AppConfig.PREF_VPN_DNS) ?: AppConfig.DNS_VPN
+        return vpnDns.split(",").filter { Utils.isPureIpAddress(it) }
+        // allow empty, in that case dns will use system default
+    }
+
+
+    fun getDelayTestUrl(second: Boolean = false): String {
+        return if (second) {
+            AppConfig.DelayTestUrl2
+        } else {
+            MmkvManager.decodeSettingsString(AppConfig.PREF_DELAY_TEST_URL)
+                ?: AppConfig.DelayTestUrl
+        }
+    }
+
+    fun getLocale(): Locale {
+        val langCode =
+            MmkvManager.decodeSettingsString(AppConfig.PREF_LANGUAGE) ?: Language.AUTO.code
+        val language = Language.fromCode(langCode)
+
+        return when (language) {
+            Language.AUTO -> Utils.getSysLocale()
+            Language.ENGLISH -> Locale.ENGLISH
+            Language.CHINA -> Locale.CHINA
+            Language.TRADITIONAL_CHINESE -> Locale.TRADITIONAL_CHINESE
+            Language.VIETNAMESE -> Locale("vi")
+            Language.RUSSIAN -> Locale("ru")
+            Language.PERSIAN -> Locale("fa")
+            Language.BANGLA -> Locale("bn")
+            Language.BAKHTIARI -> Locale("bqi", "IR")
+        }
+    }
+
+    fun setNightMode() {
+        when (MmkvManager.decodeSettingsString(AppConfig.PREF_UI_MODE_NIGHT, "0")) {
+            "0" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            "1" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            "2" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
     }
 }

V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayProxyOnlyService.kt
@@ -6,8 +6,8 @@ import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
+import com.v2ray.ang.handler.SettingsManager
 import com.v2ray.ang.util.MyContextWrapper
-import com.v2ray.ang.util.Utils
 import java.lang.ref.SoftReference
 
 class V2RayProxyOnlyService : Service(), ServiceControl {
@@ -49,7 +49,7 @@ class V2RayProxyOnlyService : Service(), ServiceControl {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun attachBaseContext(newBase: Context?) {
         val context = newBase?.let {
-            MyContextWrapper.wrap(newBase, Utils.getLocale())
+            MyContextWrapper.wrap(newBase, SettingsManager.getLocale())
         }
         super.attachBaseContext(context)
     }

V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayServiceManager.kt
@@ -15,6 +15,7 @@ import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.handler.MmkvManager
+import com.v2ray.ang.handler.SettingsManager
 import com.v2ray.ang.handler.V2rayConfigManager
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.PluginUtil
@@ -176,14 +177,14 @@ object V2RayServiceManager {
             var errstr = ""
             if (v2rayPoint.isRunning) {
                 try {
-                    time = v2rayPoint.measureDelay(Utils.getDelayTestUrl())
+                    time = v2rayPoint.measureDelay(SettingsManager.getDelayTestUrl())
                 } catch (e: Exception) {
                     Log.d(ANG_PACKAGE, "measureV2rayDelay: $e")
                     errstr = e.message?.substringAfter("\":") ?: "empty message"
                 }
                 if (time == -1L) {
                     try {
-                        time = v2rayPoint.measureDelay(Utils.getDelayTestUrl(true))
+                        time = v2rayPoint.measureDelay(SettingsManager.getDelayTestUrl(true))
                     } catch (e: Exception) {
                         Log.d(ANG_PACKAGE, "measureV2rayDelay: $e")
                         errstr = e.message?.substringAfter("\":") ?: "empty message"

V2rayNG/app/src/main/java/com/v2ray/ang/ui/BaseActivity.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import com.v2ray.ang.handler.SettingsManager
 import com.v2ray.ang.util.MyContextWrapper
 import com.v2ray.ang.util.Utils
 
@@ -34,6 +35,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun attachBaseContext(newBase: Context?) {
-        super.attachBaseContext(MyContextWrapper.wrap(newBase ?: return, Utils.getLocale()))
+        super.attachBaseContext(MyContextWrapper.wrap(newBase ?: return, SettingsManager.getLocale()))
     }
 }

V2rayNG/app/src/main/java/com/v2ray/ang/util/SpeedtestUtil.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.extension.responseLength
+import com.v2ray.ang.handler.SettingsManager
 import kotlinx.coroutines.isActive
 import libv2ray.Libv2ray
 import java.io.IOException
@@ -35,7 +36,7 @@ object SpeedtestUtil {
 
     fun realPing(config: String): Long {
         return try {
-            Libv2ray.measureOutboundDelay(config, Utils.getDelayTestUrl())
+            Libv2ray.measureOutboundDelay(config, SettingsManager.getDelayTestUrl())
         } catch (e: Exception) {
             Log.d(AppConfig.ANG_PACKAGE, "realPing: $e")
             -1L
@@ -98,7 +99,7 @@ object SpeedtestUtil {
         var result: String
         var elapsed = -1L
 
-        val conn = HttpUtil.createProxyConnection(Utils.getDelayTestUrl(), port, 30000, 30000) ?: return Pair(elapsed, "")
+        val conn = HttpUtil.createProxyConnection(SettingsManager.getDelayTestUrl(), port, 30000, 30000) ?: return Pair(elapsed, "")
         try {
             val start = SystemClock.elapsedRealtime()
             val code = conn.responseCode

UWU-TEAM/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/handler/SettingsManager.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.content.res.AssetManager
 import android.text.TextUtils
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.AppConfig.GEOIP_PRIVATE
 import com.neko.v2ray.AppConfig.GEOSITE_PRIVATE
 import com.neko.v2ray.AppConfig.TAG_DIRECT
 import com.neko.v2ray.dto.EConfigType
+import com.neko.v2ray.dto.Language
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.RoutingType
 import com.neko.v2ray.dto.RulesetItem
@@ -18,10 +20,10 @@ import com.neko.v2ray.handler.MmkvManager.decodeServerConfig
 import com.neko.v2ray.handler.MmkvManager.decodeServerList
 import com.neko.v2ray.util.JsonUtil
 import com.neko.v2ray.util.Utils
-import com.neko.v2ray.util.Utils.parseInt
 import java.io.File
 import java.io.FileOutputStream
 import java.util.Collections
+import java.util.Locale
 
 object SettingsManager {
 
@@ -173,7 +175,7 @@ object SettingsManager {
     }
 
     fun getSocksPort(): Int {
-        return parseInt(MmkvManager.decodeSettingsString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
+        return Utils.parseInt(MmkvManager.decodeSettingsString(AppConfig.PREF_SOCKS_PORT), AppConfig.PORT_SOCKS.toInt())
     }
 
     fun getHttpPort(): Int {
@@ -204,5 +206,76 @@ object SettingsManager {
             Log.e(ANG_PACKAGE, "asset copy failed", e)
         }
 
+    }
+
+    /**
+     * get domestic dns servers from preference
+     */
+    fun getDomesticDnsServers(): List<String> {
+        val domesticDns =
+            MmkvManager.decodeSettingsString(AppConfig.PREF_DOMESTIC_DNS) ?: AppConfig.DNS_DIRECT
+        val ret = domesticDns.split(",").filter { Utils.isPureIpAddress(it) || Utils.isCoreDNSAddress(it) }
+        if (ret.isEmpty()) {
+            return listOf(AppConfig.DNS_DIRECT)
+        }
+        return ret
+    }
+
+    /**
+     * get remote dns servers from preference
+     */
+    fun getRemoteDnsServers(): List<String> {
+        val remoteDns =
+            MmkvManager.decodeSettingsString(AppConfig.PREF_REMOTE_DNS) ?: AppConfig.DNS_PROXY
+        val ret = remoteDns.split(",").filter { Utils.isPureIpAddress(it) || Utils.isCoreDNSAddress(it) }
+        if (ret.isEmpty()) {
+            return listOf(AppConfig.DNS_PROXY)
+        }
+        return ret
+    }
+
+    /**
+     * get vpn dns servers from preference
+     */
+    fun getVpnDnsServers(): List<String> {
+        val vpnDns = MmkvManager.decodeSettingsString(AppConfig.PREF_VPN_DNS) ?: AppConfig.DNS_VPN
+        return vpnDns.split(",").filter { Utils.isPureIpAddress(it) }
+        // allow empty, in that case dns will use system default
+    }
+
+
+    fun getDelayTestUrl(second: Boolean = false): String {
+        return if (second) {
+            AppConfig.DelayTestUrl2
+        } else {
+            MmkvManager.decodeSettingsString(AppConfig.PREF_DELAY_TEST_URL)
+                ?: AppConfig.DelayTestUrl
+        }
+    }
+
+    fun getLocale(): Locale {
+        val langCode =
+            MmkvManager.decodeSettingsString(AppConfig.PREF_LANGUAGE) ?: Language.AUTO.code
+        val language = Language.fromCode(langCode)
+
+        return when (language) {
+            Language.AUTO -> Utils.getSysLocale()
+            Language.ENGLISH -> Locale.ENGLISH
+            Language.CHINA -> Locale.CHINA
+            Language.TRADITIONAL_CHINESE -> Locale.TRADITIONAL_CHINESE
+            Language.VIETNAMESE -> Locale("vi")
+            Language.RUSSIAN -> Locale("ru")
+            Language.PERSIAN -> Locale("fa")
+            Language.BANGLA -> Locale("bn")
+            Language.BAKHTIARI -> Locale("bqi", "IR")
+        }
+    }
+
+    fun setNightMode() {
+        when (MmkvManager.decodeSettingsString(AppConfig.PREF_UI_MODE_NIGHT, "0")) {
+            "0" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            "1" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            "2" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
     }
 }

V2rayNG/app/src/main/java/com/neko/v2ray/service/V2RayProxyOnlyService.kt
@@ -6,8 +6,8 @@ import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import androidx.annotation.RequiresApi
+import com.neko.v2ray.handler.SettingsManager
 import com.neko.v2ray.util.MyContextWrapper
-import com.neko.v2ray.util.Utils
 import java.lang.ref.SoftReference
 
 class V2RayProxyOnlyService : Service(), ServiceControl {
@@ -49,7 +49,7 @@ class V2RayProxyOnlyService : Service(), ServiceControl {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun attachBaseContext(newBase: Context?) {
         val context = newBase?.let {
-            MyContextWrapper.wrap(newBase, Utils.getLocale())
+            MyContextWrapper.wrap(newBase, SettingsManager.getLocale())
         }
         super.attachBaseContext(context)
     }

V2rayNG/app/src/main/java/com/neko/v2ray/service/V2RayServiceManager.kt
@@ -15,6 +15,7 @@ import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.handler.MmkvManager
+import com.neko.v2ray.handler.SettingsManager
 import com.neko.v2ray.handler.V2rayConfigManager
 import com.neko.v2ray.util.MessageUtil
 import com.neko.v2ray.util.PluginUtil
@@ -176,14 +177,14 @@ object V2RayServiceManager {
             var errstr = ""
             if (v2rayPoint.isRunning) {
                 try {
-                    time = v2rayPoint.measureDelay(Utils.getDelayTestUrl())
+                    time = v2rayPoint.measureDelay(SettingsManager.getDelayTestUrl())
                 } catch (e: Exception) {
                     Log.d(ANG_PACKAGE, "measureV2rayDelay: $e")
                     errstr = e.message?.substringAfter("\":") ?: "empty message"
                 }
                 if (time == -1L) {
                     try {
-                        time = v2rayPoint.measureDelay(Utils.getDelayTestUrl(true))
+                        time = v2rayPoint.measureDelay(SettingsManager.getDelayTestUrl(true))
                     } catch (e: Exception) {
                         Log.d(ANG_PACKAGE, "measureV2rayDelay: $e")
                         errstr = e.message?.substringAfter("\":") ?: "empty message"

V2rayNG/app/src/main/java/com/neko/v2ray/ui/BaseActivity.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.neko.themeengine.ThemeEngine
+import androidx.core.view.WindowCompat
+import com.neko.v2ray.handler.SettingsManager
 import com.neko.v2ray.util.MyContextWrapper
 import com.neko.v2ray.util.Utils
-import com.neko.themeengine.ThemeEngine
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -31,6 +33,6 @@ abstract class BaseActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun attachBaseContext(newBase: Context?) {
-        super.attachBaseContext(MyContextWrapper.wrap(newBase ?: return, Utils.getLocale()))
+        super.attachBaseContext(MyContextWrapper.wrap(newBase ?: return, SettingsManager.getLocale()))
     }
 }

V2rayNG/app/src/main/java/com/neko/v2ray/util/SpeedtestUtil.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.extension.responseLength
+import com.neko.v2ray.handler.SettingsManager
 import kotlinx.coroutines.isActive
 import libv2ray.Libv2ray
 import java.io.IOException
@@ -35,7 +36,7 @@ object SpeedtestUtil {
 
     fun realPing(config: String): Long {
         return try {
-            Libv2ray.measureOutboundDelay(config, Utils.getDelayTestUrl())
+            Libv2ray.measureOutboundDelay(config, SettingsManager.getDelayTestUrl())
         } catch (e: Exception) {
             Log.d(AppConfig.ANG_PACKAGE, "realPing: $e")
             -1L
@@ -98,7 +99,7 @@ object SpeedtestUtil {
         var result: String
         var elapsed = -1L
 
-        val conn = HttpUtil.createProxyConnection(Utils.getDelayTestUrl(), port, 30000, 30000) ?: return Pair(elapsed, "")
+        val conn = HttpUtil.createProxyConnection(SettingsManager.getDelayTestUrl(), port, 30000, 30000) ?: return Pair(elapsed, "")
         try {
             val start = SystemClock.elapsedRealtime()
             val code = conn.responseCode